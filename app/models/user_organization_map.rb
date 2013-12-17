31defccc12d18bc6db2ba1c7cfd0c5f5037c4e52
Julian Ray <jray@camsys.com>
2013-12-17 12:28:23 -0500

    Missing class

---
+++ app/models/user_organization_map.rb
@@ -0,0 +1,10 @@
+class UserOrganizationMap < ActiveRecord::Base
+
+  # Associations
+  belongs_to :user
+  belongs_to :organization
+  
+  validates :user_id, :presence => true
+  validates :organization_id, :presence => true
+  
+end
