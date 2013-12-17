9a109d5baf775525d48f56660d403bee91bc6022
Julian Ray <jray@camsys.com>
2013-12-17 13:56:05 -0500

    added layout to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,34 @@ class SessionsController < Devise::SessionsController
   after_filter  :clear_flash_messages, :only => [:create, :destroy]  
   before_filter :log_logout, :only => :destroy  #add this at the top with the other filters
 
-# POST /resource/sign_in
+  # determine which layout to use based on the current user state
+  layout :layout_by_resource
+
+  # Determine which layout to use based on the authorized state
+  def layout_by_resource
+    if user_signed_in?
+      "application"
+    else
+      "unauthorized"
+    end
+  end       
+
+  # POST /resource/sign_in
   def create
     super
     ::Rails.logger.info "\n***\nSuccessful login with email : #{request.filtered_parameters["email"]}\n***\n"
   end
 
-protected
+  protected
+  
   def clear_flash_messages
     if flash.keys.include?(:notice)
       flash.delete(:notice)
     end
   end
 
-private
-
+  private
+  
   def log_failed_login
     ::Rails.logger.info "\n***\nFailed login with email : #{request.filtered_parameters["email"]}\n***\n" if failed_login?
   end 
