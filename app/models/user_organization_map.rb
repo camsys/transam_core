13ad9ac25bc9c3d6adb550290fde6cd1558f5feb
Julian Ray <jray@camsys.com>
2013-12-17 12:48:46 -0500

    Fixed validation for table mapping

--- app/models/user_organization_map.rb
+++ app/models/user_organization_map.rb
@@ -4,7 +4,7 @@ class UserOrganizationMap < ActiveRecord::Base
   belongs_to :user
   belongs_to :organization
   
-  validates :user_id, :presence => true
-  validates :organization_id, :presence => true
+  #validates :user_id, :presence => true
+  #validates :organization_id, :presence => true
   
 end
