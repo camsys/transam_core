f1a9247f4c54cfdaa325e2a24ab9c575f503ad64
Julian Ray <jray@camsys.com>
2013-12-18 07:56:18 -0500

    More namspacing

---
+++ app/assets/javascripts/transam_core/transam.js
@@ -0,0 +1,124 @@
+//
+// Transam specific javascript
+//
+var transam = new function() {
+
+	this.ajax_render_action = function(url, method) {
+		$.ajax({
+	    	type: method,
+	      	url: url,
+	      	beforeSend:function(){
+	        	// this is where we append a loading image
+	    		//$('#ajax-panel').html('<div class="loading"><img src="/images/loading.gif" alt="Loading..." /></div>');
+	  		},
+	  		success:function(data){
+			    // successful request; do something with the data
+			    //$('#ajax-panel').empty();
+			    //$(data).find('item').each(function(i){
+			    //  $('#ajax-panel').append('<h4>' + $(this).find('title').text() + '</h4><p>' + $(this).find('link').text() + '</p>');
+			    //});
+			},
+			error: function (data) {
+	      		show_alert("We are sorry but something went wrong. Please try again.");                
+	      	}
+	   	});  
+	};
+
+	//
+	// session based variable storage for UI key/value pairs
+	//
+	// get a key value, if the key does not exist the default value is returned  
+	this.get_ui_key_value = function(key, default_val) {
+	    var value;
+	    try {
+	        value = window.sessionStorage.getItem(key);
+	    } catch(e) {
+	        value = default_val;
+	    }    
+		//alert('getting value for ' + key + '; val = ' + value);
+	    return value;
+	};
+	
+	// Set a key value. Keys must be unique strings
+	this. set_ui_key_value = function(key, value) {
+		//alert('setting value for ' + key + ' to ' + value);
+	    window.sessionStorage.setItem(key, value);
+	};
+
+	// Used to remove any existing banner messages
+	this.remove_messages = function() {
+		$('.alert').alert('close');
+	};
+
+	//
+	// Adds the first-in-row class to a list of thumbnails. Assumes that
+	// the view is using the standard 12 column bootstrap layout
+	// span_size is the size of the span* classes added to each
+	// thumbnail 
+	this.fix_thumbnail_margins = function(span_size, class_name) {
+
+		var counter = 12 / span_size;
+		var selector = class_name == null ? '.thumbnail' : '.' + class_name;
+		var i = 0;
+		$(selector).each(function() {
+			var remainder = i % counter;
+			//alert('i = ' + i + ' remainder = ' + remainder);
+			if (remainder == 0) {
+				$(this).addClass('first-in-row');
+			}
+			i++;
+		});	
+	};
+
+	this.enable_date_pickers = function() {
+		// Use jquery to render date pickers for all datepicker input classes
+		$('.datepicker').datepicker({
+			 format: 'yyyy-mm-dd'
+		});
+	};
+
+	// Finds all the class elements on a page and sets the min-height css variable
+	// to the maximum height of all the containers
+	this.make_same_height = function(class_name) {
+	
+		// remove any existing min-height attributes
+		$(class_name).css({'min-height' : ''});
+		
+		// Set the form parts to equal height
+		var max = -1;
+		$(class_name).each(function() {
+			var h = $(this).height(); 
+			//alert(h);
+			max = h > max ? h : max;
+		});
+		max += 10;
+		$(class_name).css({'min-height': max});	
+	};
+
+	// Finds all elements marked as info icons and turns them into popups
+	this.enable_info_popups = function(class_name) {
+		$(class_name).popover({
+			trigger: 'hover'
+		});
+	};
+
+	this.install_quick_link_handlers = function() {
+		// Enable the quick links
+		$('[data-action-path]').click(function() {
+			var url = $(this).data('action-path');
+			document.location.href = url;
+		});		
+	};
+
+
+    var internalFunction = function() {
+
+    };
+
+    this.publicFunction = function() {
+
+    };
+};
+
+
+
