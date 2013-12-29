25bb5cdb7103bd079694532f91132957b61e462b
Julian Ray <jray@camsys.com>
2013-12-29 11:03:03 -0500

    Added asset type to manufacturer lookup

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,7 +1,10 @@
 class Manufacturer < ActiveRecord::Base
+  
+  # every manufacturuer is specific to an asset class
+  belongs_to  :asset_type
     
   # every manufacturer can have 0 or more models
-  has_many :manufacturer_models
+  has_many    :manufacturer_models
   
   #attr_accessible :name, :description, :code, :active
         
