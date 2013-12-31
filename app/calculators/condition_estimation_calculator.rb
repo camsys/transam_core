5eaa0e5620e4a611c0f4f5c405a52e968207cb2c
Julian Ray <jray@camsys.com>
2013-12-31 10:58:46 -0500

    Added missing class

---
+++ app/calculators/condition_estimation_calculator.rb
@@ -0,0 +1,8 @@
+class ConditionEstimationCalculator < Calculator
+  
+  # All class instances should override this method to return the projected last year for an asset 
+  def last_year(asset)
+    nil
+  end  
+  
+end
