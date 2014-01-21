d9268ebf02d59c9b451b08c4ed4b0d701494c595
Julian Ray <jray@camsys.com>
2014-01-21 07:07:55 -0500

    Make manufacturers many to many with asset_types

--- app/models/asset_type.rb
+++ app/models/asset_type.rb
@@ -6,12 +6,8 @@ class AssetType < ActiveRecord::Base
   has_many :asset_subtypes
 
   # every asset type has 0 or more manufacturers
-  has_many :manufacturers
-  
-  #attr_accessible :name, :description, :active
-  #attr_accessible :class_name, :form_name, :index_name
-  #attr_accessible :map_icon_name, :display_icon_name
-  
+  has_and_belongs_to_many :manufacturers
+    
   # default scope
   default_scope { where(:active => true) }
   
--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,10 +1,5 @@
 class Manufacturer < ActiveRecord::Base
-  
-  # every manufacturuer is specific to an asset class
-  belongs_to  :asset_type
-      
-  #attr_accessible :name, :description, :code, :active
-        
+          
   # default scope
   default_scope { where(:active => true) }
 
