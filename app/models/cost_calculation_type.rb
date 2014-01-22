e790edbabc425c6542dea66d8e8110bbe8700cdc
Julian Ray <jray@camsys.com>
2014-01-22 07:24:29 -0500

    Remove association from lookup table

--- app/models/cost_calculation_type.rb
+++ app/models/cost_calculation_type.rb
@@ -1,10 +1,5 @@
 class CostCalculationType < ActiveRecord::Base
-  
-  # associations
-  has_many :policys
-
-  #attr_accessible :name, :description, :class_name, :active
-        
+          
   # default scope
   default_scope { where(:active => true) }
         
