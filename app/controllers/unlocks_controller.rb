1c5e3b8be4d046217503e7b799782823f95f1462
Julian Ray <jray@camsys.com>
2014-02-03 09:32:44 -0500

    Adding devise controllers and views for resetting passwords and unlocking accounts

---
+++ app/controllers/unlocks_controller.rb
@@ -0,0 +1,15 @@
+class UnlocksController < Devise::UnlocksController
+
+  # determine which layout to use based on the current user state
+  layout :layout_by_resource
+
+  # Determine which layout to use based on the authorized state
+  def layout_by_resource
+    if user_signed_in?
+      "application"
+    else
+      "unauthorized"
+    end
+  end       
+ 
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :lockable, :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   # Include the unique key mixin
   include UniqueKey
