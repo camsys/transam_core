38c2db9162933225f955be1b6e3f630cf99f5849
Julian Ray <jray@camsys.com>
2014-02-04 19:03:01 -0500

    Fix messages on dashboard to show only new messages and link to message controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,7 @@ class DashboardsController < OrganizationAwareController
     @page_title = 'Dashboard'
     
     # Select messages for this user or ones that are for the agency as a whole
-    @messages = Message.where("organization_id = ? AND thread_message_id IS NULL AND (to_user_id IS NULL OR to_user_id = ?)", @organization.id, current_user.id).order("created_at DESC")
+    @messages = Message.where("organization_id = ? AND to_user_id = ? AND opened_at IS NULL)", @organization.id, current_user.id).order("created_at DESC")
     # Select tasks for this user or ones that are for the agency as a whole
     @tasks = Task.where("for_organization_id = ? AND completed_on IS NULL AND (assigned_to_user_id IS NULL OR assigned_to_user_id = ?)", @organization.id, current_user.id).order("complete_by")
             
