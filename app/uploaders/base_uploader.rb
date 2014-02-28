da50ab67eefcf2c4f4952ab98e812704932f05b0
Julian Ray <jray@camsys.com>
2014-02-28 10:50:17 -0500

    Tweking config

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -1,12 +1,7 @@
 class BaseUploader < TransamAbstractUploader
   
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  #storage :fog
-
-  # Configure the cache folder
-  #def cache_dir
-  #  "#{Rails.root}/tmp/uploads"
-  #end
+  #storage :file
+  storage :fog
   
 end
