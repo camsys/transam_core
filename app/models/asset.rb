68bef85b026834956f9a0658a26495b75c5b008f
Julian Ray <jray@camsys.com>
2014-03-18 12:34:03 -0400

    Require every asset to identify its year of manufacture

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -83,6 +83,7 @@ class Asset < ActiveRecord::Base
   validates     :asset_type_id,       :presence => true
   validates     :asset_subtype_id,    :presence => true
   validates     :created_by_id,       :presence => true
+  validates     :manufacture_year,    :presence => true
   validates_numericality_of :manufacture_year,    :only_integer => :true,   :greater_than_or_equal_to => 1900
     
   #------------------------------------------------------------------------------
