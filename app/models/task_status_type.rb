d87d2fb03fe7a54b9f0c28287b338cf3d3756df8
Julian Ray <jray@camsys.com>
2014-03-19 10:14:21 -0400

    Add task status type model

---
+++ app/models/task_status_type.rb
@@ -0,0 +1,13 @@
+class TaskStatusType < ActiveRecord::Base
+    
+  #attr_accessible :name, :description, :code, :active
+        
+  # default scope
+  default_scope { where(:active => true) }
+
+  def to_s
+    name
+  end
+
+end
+
