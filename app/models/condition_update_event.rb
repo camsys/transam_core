7a939fda85bb6a9a06218caf88ac78cf9907077b
Julian Ray <jray@camsys.com>
2014-03-19 09:44:12 -0400

    Fix validator for condition update event [fixes #67029998]

--- app/models/condition_update_event.rb
+++ app/models/condition_update_event.rb
@@ -16,11 +16,11 @@ class ConditionUpdateEvent < AssetEvent
   #attr_accessible :assessed_rating
       
   validates                 :condition_type_id, :presence => true
-  validates_numericality_of :assessed_rating, :greater_than_or_equal_to => 0,   :less_than_or_equal_to => 5
+  validates_numericality_of :assessed_rating, :greater_than_or_equal_to => 0,   :less_than_or_equal_to => 5, :allow_nil => :true
   validates_numericality_of :current_mileage, :greater_than_or_equal_to => 0,   :less_than_or_equal_to => 1000000,  :only_integer => :true,   :allow_nil => :true
   
   before_validation do
-    self.assessed_rating ||= ConditionType.find(condition_type_id).rating
+    self.assessed_rating ||= ConditionType.find(condition_type_id).rating unless condition_type_id.blank?
   end
     
   #------------------------------------------------------------------------------
