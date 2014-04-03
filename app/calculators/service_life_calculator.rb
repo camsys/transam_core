223011c5f774025ada7c07130bfe4f4da67fbf07
Julian Ray <jray@camsys.com>
2014-04-03 12:53:37 -0400

    Fix a few issues in the service life age and condition calculator

--- app/calculators/service_life_calculator.rb
+++ app/calculators/service_life_calculator.rb
@@ -22,18 +22,27 @@ class ServiceLifeCalculator < Calculator
     # Iterate over all the condition update events from earliest to latest
     # and find the first year (if any) that the  policy replacement became
     # effective
-    asset.condition_updates.each do |event|
-      if event.assessed_rating < @policy.condition_threshold
+    policy_item = @policy.get_policy_item(asset)
+    events = asset.condition_updates(true)
+    Rails.logger.debug "Found #{events.count} events."
+    Rails.logger.debug "Condition threshold = #{@policy.condition_threshold} max_service_life_miles = #{policy_item.max_service_life_miles}."
+    events.each do |event|
+      Rails.logger.debug "Event date = #{event.event_date}, Rating = #{event.assessed_rating}, Mileage = #{event.current_mileage}"
+      if event.assessed_rating <= @policy.condition_threshold
+        Rails.logger.debug "returning #{event.event_date.year}"
         return event.event_date.year
       end
       if event.current_mileage && policy_item.max_service_life_miles
         if event.current_mileage >= policy_item.max_service_life_miles
+          Rails.logger.debug "returning #{event.event_date.year}"
           return event.event_date.year
         end
       end
     end
     # if we didn't find a condition event that would make the policy effective
     # we can simply return the age constraint
+    Rails.logger.debug "returning value from policy age"
+
     by_age(asset)
   end  
   
