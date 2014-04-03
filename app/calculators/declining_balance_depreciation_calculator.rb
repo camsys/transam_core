b761413697493221e3ee7a8cdfc032cda1743ff7
Julian Ray <jray@camsys.com>
2014-04-03 06:43:33 -0400

    Fixed issues with declining balance calculator. Tests now pass

--- app/calculators/declining_balance_depreciation_calculator.rb
+++ app/calculators/declining_balance_depreciation_calculator.rb
@@ -10,19 +10,28 @@ class DecliningBalanceDepreciationCalculator < DepreciationCalculator
 
     # depreciation time
     num_years = @policy.get_policy_item(asset).max_service_life_years
-    
+    # Age of the asset
+    asset_age = asset.age
+    Rails.logger.debug "Age = #{asset.age}, max service life = #{num_years}"
     # calculate the annual depreciation rate. This is double the actual depreciation rate
     depreciation_rate = (1 / num_years.to_f) * 2
     rv = residual_value(asset)
     v  = purchase_cost(asset)
+    Rails.logger.debug "purchase cost = #{v}, residual value = #{rv} depreciation_rate = #{depreciation_rate}"
+    
+    if asset_age < 1
+      return v
+    end
+    
     # calculate the value of the asset at the end of each year
-    [0..asset.age].each do |year|
+    (1..asset.age).each do |year|
       v -= (v * depreciation_rate)
+      Rails.logger.debug "year = #{year}, value = #{v}"
       # if the value drops below the residual value then the depreciation stops
       break if v < rv
     end
     # return the max of the residual value and the depreciated value
-    [v, rv].max
+    [v, rv].max.to_i
   end
   
 end
