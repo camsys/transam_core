f302de8c214940eae1239490fe3bd6d026f1f8e9
Julian Ray <jray@camsys.com>
2014-04-04 15:22:16 -0400

    Add search method to models

--- app/models/district.rb
+++ app/models/district.rb
@@ -21,4 +21,14 @@ class District < ActiveRecord::Base
     name
   end
   
+  def self.search(text, exact = true)
+    if exact
+      x = where('name = ? OR code = ? OR description = ?', text, text, text).first
+    else
+      val = "%#{text}%"
+      x = where('name = LIKE ? OR code LIKE ? OR description LIKE ?', val, val, val).first
+    end
+    x
+  end
+  
 end
