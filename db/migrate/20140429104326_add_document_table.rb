6af8956d48c4f866bf1e9e588793ba12987b8eff
Julian Ray <jray@camsys.com>
2014-04-29 14:44:20 -0400

    Fix migration

--- db/migrate/20140429104326_add_document_table.rb
+++ db/migrate/20140429104326_add_document_table.rb
@@ -6,10 +6,10 @@ class AddDocumentTable < ActiveRecord::Migration
       t.string    :object_key,        :limit => 12,   :null => :false
       t.integer   :documentable_id,                   :null => :false
       t.string    :documentable_type,                 :null => :false
-      t.string    :name,              :limit => 64,   :null => :false
+      t.string    :document,          :limit => 128,  :null => :false
       t.string    :description
       t.string    :original_filename, :limit => 128,  :null => :false
-      t.string    :content_type,      :limit => 64,   :null => :false
+      t.string    :content_type,      :limit => 128,  :null => :false
       t.integer   :file_size
       t.integer   :created_by_id,                     :null => :false      
       t.timestamps
