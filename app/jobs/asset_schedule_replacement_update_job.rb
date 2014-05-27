8d32d124bd5e0b1ca0181fbebffe62043d9d7cc2
Julian Ray <jray@camsys.com>
2014-05-27 10:34:00 -0400

    Add update job for scheduled replacement/rebuilds

--- app/jobs/asset_schedule_replacement_update_job.rb
+++ app/jobs/asset_schedule_replacement_update_job.rb
@@ -1,30 +1,25 @@
 #------------------------------------------------------------------------------
 #
-# AssetUpdateJob
+# AssetScheduleReplacementUpdateJob
 #
-# Updates all componetns of an asset
+# Updates an assets scheduled replacement/rehabilitation
 #
 #------------------------------------------------------------------------------
-class AssetUpdateJob < Job
+class AssetScheduleReplacementUpdateJob < Job
   
   attr_accessor :object_key
   
   def run    
     asset = Asset.find_by_object_key(object_key)
     if asset
-      asset.update_condition
-      asset.update_mileage
-      asset.update_location
       asset.update_scheduled_replacement
-      asset.update_service_status
-      asset.update_estimated_value
     else
       raise RuntimeError, "Can't find Asset with object_key #{object_key}"
     end
   end
 
   def prepare
-    Rails.logger.debug "Executing AssetUpdateJob at #{Time.now.to_s} for Asset #{object_key}"    
+    Rails.logger.debug "Executing AssetScheduleReplacementUpdateJob at #{Time.now.to_s} for Asset #{object_key}"    
   end
   
   def check    
