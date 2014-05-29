23f87a3bd5bf02d3ac004cd8fa983a0106c12d95
Julian Ray <jray@camsys.com>
2014-05-29 14:27:08 -0400

    Hack nested resource controller for nested resources

--- app/controllers/nested_resource_controller.rb
+++ app/controllers/nested_resource_controller.rb
@@ -10,9 +10,14 @@ class NestedResourceController < OrganizationAwareController
   # controllers which are aliased (eg asset => inventory) get the wrong controller name
   # so we have to deal with those
   def get_resource_url(resource)
+    puts resource.inspect
     controller_name = resource.class.name.underscore
     if controller_name == 'asset'
       controller_name = 'inventory'
+      eval("#{controller_name}_url('#{resource.object_key}')")
+    elsif controller_name == 'activity_line_item'
+      controller_name = "capital_project_activity_line_item"
+      eval("#{controller_name}_url('[#{resource.capital_project.object_key},#{resource.object_key}]')")
     end
     eval("#{controller_name}_url('#{resource.object_key}')")
   end
@@ -21,7 +26,7 @@ class NestedResourceController < OrganizationAwareController
   # case where the controller is aliased and we need to determine this and replace the correct
   # resource class name
   def find_resource
-    params.each do |name, value|
+    params.reverse_each do |name, value|
       if name =~ /(.+)_id$/
         if $1 == 'inventory'
           return 'asset'.classify.constantize.find_by_object_key(value)
