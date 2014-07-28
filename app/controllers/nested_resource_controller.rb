b8c8d947b1702b117aaa515889c78522b283becf
Julian Ray <jray@camsys.com>
2014-07-28 14:15:40 -0400

    Remove ALI logic from generic nested resource controller [fixes #75802464]

--- app/controllers/nested_resource_controller.rb
+++ app/controllers/nested_resource_controller.rb
@@ -15,12 +15,8 @@ class NestedResourceController < OrganizationAwareController
     #puts controller_name
     if controller_name == 'asset'
       controller_name = 'inventory'
-      eval("#{controller_name}_url('#{resource.object_key}')")
-    elsif controller_name == 'activity_line_item'
-      capital_project_activity_line_item_path(resource.capital_project, resource)
-    else
-      eval("#{controller_name}_url('#{resource.object_key}')")
     end
+    eval("#{controller_name}_url('#{resource.object_key}')")
   end
   
   # Get the class and object key of the commentable object we are operating on. There is a special
