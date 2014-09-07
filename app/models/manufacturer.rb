3903c5751224611c319ef44ee1160e3d6e5ac1ea
Aaron Magil <amagil@camsys.com>
2014-09-06 23:44:20 -0400

    [Fixes#77376318] Improves display of manufacturer name.

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -3,6 +3,10 @@ class Manufacturer < ActiveRecord::Base
   # default scope
   default_scope { where(:active => true).order('code') }
 
+  def full_name
+    "#{name} - #{filter.titleize}"
+  end
+
   def to_s
     name
   end
