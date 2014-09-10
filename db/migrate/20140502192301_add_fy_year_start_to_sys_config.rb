010a43d175de6f30ed854a60b06a2fe320fdd003
Julian Ray <jray@camsys.com>
2014-09-10 11:03:31 -0400

    Update core engine to remove transit-specific artifacts and create rspec test environment

--- db/migrate/20140502192301_add_fy_year_start_to_sys_config.rb
+++
@@ -1,5 +0,0 @@
-class AddFyYearStartToSysConfig < ActiveRecord::Migration
-  def change
-    add_column  :system_configs, :start_of_fiscal_year,  :string, :limit => 5, :after => :customer_id
-  end
-end
