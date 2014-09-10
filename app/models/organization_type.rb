c643a391448b434e2707dd7c002fde253715c3da
Julian Ray <jray@camsys.com>
2014-09-10 14:42:37 -0400

    Continued cleanup of core

--- app/models/organization_type.rb
+++ app/models/organization_type.rb
@@ -1,8 +1,5 @@
 class OrganizationType < ActiveRecord::Base
-  
-  #attr_accessible :class_name
-  #attr_accessible :name, :description, :active, :display_icon_name
-        
+          
   # default scope
   default_scope { where(:active => true) }
   
