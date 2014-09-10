294a65ecbb4cbf147ba74244235b7b7bab641247
Julian Ray <jray@camsys.com>
2014-09-10 11:03:31 -0400

    Update core engine to remove transit-specific artifacts and create rspec test environment

--- db/migrate/20140527141020_update_asset_events.rb
+++
@@ -1,7 +0,0 @@
-class UpdateAssetEvents < ActiveRecord::Migration
-  def change
-    add_column  :asset_events, :location_id,        :integer, :after => :current_mileage
-    add_column  :asset_events, :replacement_year,   :integer, :after => :location_id
-    add_column  :asset_events, :rebuild_year,       :integer, :after => :replacement_year
-  end
-end
