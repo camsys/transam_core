efee4101ffe246f1beb9b6c6d6dc6309b3faafff
Aaron Magil <amagil@camsys.com>
2014-09-15 11:35:45 -0400

    Fixes typo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ActiveRecord::Base
 
   #associations
   belongs_to  :license_type
-  has_one     :license_holder, -> { where('license_holder = true')} :class_name => 'Organization'
+  has_one     :license_holder, -> { where('license_holder = true')}, :class_name => 'Organization'
   has_many    :organizations
   has_many    :users
     
--- lib/transam_core/version.rb
+++ lib/transam_core/version.rb
@@ -1,3 +1,3 @@
 module TransamCore
-  VERSION = "0.4.0"
+  VERSION = "0.4.0a"
 end
