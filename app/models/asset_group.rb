a61d1b35b623e9d77dc944467df71c3e995ed1d5
Lydia Chang <lchang@changl1.camsys.local>
2014-10-07 16:35:17 -0400

    fix lengths of fields asset_group

--- app/models/asset_group.rb
+++ app/models/asset_group.rb
@@ -6,14 +6,14 @@
 #
 #------------------------------------------------------------------------------
 class AssetGroup < ActiveRecord::Base
-        
+
   # Include the unique key mixin
   include UniqueKey
 
   #------------------------------------------------------------------------------
   # Overrides
   #------------------------------------------------------------------------------
-  
+
   #require rails to use the asset key as the restful parameter. All URLS will be of the form
   # /asset_event/{object_key}/...
   def to_param
@@ -23,22 +23,22 @@ class AssetGroup < ActiveRecord::Base
   #------------------------------------------------------------------------------
   # Callbacks
   #------------------------------------------------------------------------------
-      
+
   # Always generate a unique asset key before saving to the database
   before_validation(:on => :create) do
     generate_unique_key(:object_key)
   end
-          
+
   # Clear the mapping table when the group is destroyed
   before_destroy { assets.clear }
-          
+
   #------------------------------------------------------------------------------
   # Associations
   #------------------------------------------------------------------------------
 
   # Every asset group is owned by an organization
   belongs_to :organization
-    
+
   # Every asset grouop has zero or more assets
   has_and_belongs_to_many :assets
 
@@ -52,41 +52,41 @@ class AssetGroup < ActiveRecord::Base
   #------------------------------------------------------------------------------
   # Validations
   #------------------------------------------------------------------------------
-  
+
   validates :object_key,            :presence => true, :uniqueness => true
   validates :organization,          :presence => true
-  validates :name,                  :presence => true
-  validates :code,                  :presence => true
+  validates :name,                  :presence => true, :length => { maximum: 64 }
+  validates :code,                  :presence => true, :length => { maximum: 8 }
   validates :description,           :presence => true
-  
+
   # List of hash parameters allowed by the controller
   FORM_PARAMS = [
     :object_key,
     :organization_id,
-    :name, 
-    :code, 
-    :description, 
-    :active 
+    :name,
+    :code,
+    :description,
+    :active
   ]
-  
+
   #------------------------------------------------------------------------------
   #
   # Class Methods
   #
   #------------------------------------------------------------------------------
-    
+
   def self.allowable_params
     FORM_PARAMS
   end
-  
+
   #------------------------------------------------------------------------------
   #
   # Instance Methods
   #
   #------------------------------------------------------------------------------
-  
+
   def to_s
     name
-  end   
-  
+  end
+
 end
