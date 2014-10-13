7d0196a57d1fafb720a832f91e80795e6e9fe074
Julian Ray <jray@camsys.com>
2014-10-13 09:46:32 -0400

    Slim out the dashboards controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,24 +1,9 @@
 class DashboardsController < OrganizationAwareController
-        
+ 
+   add_breadcrumb "Home", :root_path
+       
   def index
-    
-    add_breadcrumb "Home", :root_path    
-    
-    @page_title = 'My Dashboard'
-    
-    # Select messages for this user or ones that are for the agency as a whole
-    @messages = Message.where("to_user_id = ? AND opened_at IS NULL", current_user.id).order("created_at DESC")
-
-    # Select tasks for this user or ones that are for the agency as a whole
-
-    # Get the task status types to search for
-    task_statuses = []
-    task_statuses << TaskStatusType.find_by_name('Not Started')
-    task_statuses << TaskStatusType.find_by_name('In Progress')
-    task_statuses << TaskStatusType.find_by_name('On Hold')
-            
-    @tasks = Task.where("assigned_to_user_id = ? AND task_status_type_id IN(?)", current_user.id, task_statuses).order("complete_by")
-            
+                    
   end
 
 end
