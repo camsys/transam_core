df0b17ebd1fb9aa28b2a84fc5724af91d5548303
Lydia Chang <mathmerized@gmail.com>
2014-10-24 12:58:52 -0400

    round to integers book and salvage values

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -656,8 +656,12 @@ class Asset < ActiveRecord::Base
     begin
       # see what metric we are using for the depreciated value of the asset
       class_name = policy.depreciation_calculation_type.class_name
-      asset.book_value = calculate(asset, policy, class_name, depreciated_value)
-      asset.salvage_value = calculate(asset, policy, class_name)
+
+      # caches depreciated value as an integer
+      asset.book_value = calculate(asset, policy, class_name, depreciated_value).round(0)
+
+      # caches residual value as an integer
+      asset.salvage_value = calculate(asset, policy, class_name).round(0)
     rescue Exception => e
       Rails.logger.warn e.message
     end
