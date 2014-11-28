3fd4c991c8b8781baf9c726dea5e7143bdd5e959
Julian Ray <jray@camsys.com>
2014-11-28 14:43:21 -0500

    Remove default state machines as this is getting added into concrete classes

--- lib/transam_core/transam_workflow.rb
+++ lib/transam_core/transam_workflow.rb
@@ -7,64 +7,7 @@ module TransamWorkflow
     
     validates :state, :presence => true
     
-    #------------------------------------------------------------------------------
-    #
-    # State Machine 
-    #
-    # Generic state machine for approvals
-    #
-    #------------------------------------------------------------------------------  
-    state_machine :state, :initial => :unsubmitted do
-  
-      #-------------------------------
-      # List of allowable states
-      #-------------------------------
-      
-      # initial state
-      state :unsubmitted
-      
-      # state used to signify it has been submitted and is pending approval 
-      state :pending_approval
-        
-      # state used to signify that the order has been approved by the program manager
-      state :approved
-  
-      # state used to signify that the order has been returned unaproved
-      state :returned
-  
-      #---------------------------------------------------------------------------
-      # List of allowable events. Events transition a workflow from one state to another
-      #---------------------------------------------------------------------------
-                
-      # submit for approval.
-      event :submit do
-        
-        transition [:unsubmitted, :returned] => :pending_approval
-        
-      end
-    
-      # A program manager is returning a project for additional information or changes
-      event :return do
-        
-        transition [:pending_approval] => :returned
-        
-      end    
-  
-      # A program manager is approving a project
-      event :approve do
-        
-        transition [:pending_approval] => :approved
-        
-      end    
-        
-      # Callbacks
-      before_transition do |obj, transition|
-        Rails.logger.debug "Transitioning #{obj.to_s} from #{transition.from_name} to #{transition.to_name} using #{transition.event}"
-      end       
-    end
-    
   end
-
   #------------------------------------------------------------------------------
   #
   # Class Methods
