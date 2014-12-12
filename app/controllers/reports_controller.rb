24ff9864d682d53e4b0307042e56f992d679ba13
Julian Ray <jray@camsys.com>
2014-12-12 13:50:21 -0500

    Code tweaks

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,47 +1,43 @@
 class ReportsController < OrganizationAwareController
-    
+
   before_filter :get_report, :only => [:show, :load]
-    
+
   add_breadcrumb "Home", :root_path
   add_breadcrumb "Reports", :reports_path
-  
+
   SESSION_VIEW_TYPE_VAR = 'reports_subnav_view_type'
-      
+
   def index
-    
-    @page_title = 'Reports'
-    
+
     # remember the view type
     @view_type = get_view_type(SESSION_VIEW_TYPE_VAR)
-    
+
     @reports = []
     Report.all.each do |rep|
       if current_user.is_in_roles? rep.role_names
         @reports << rep
       end
     end
-        
+
   end
 
   def load
-    
+
     report_instance = @report.class_name.constantize.new
     # inject the sql for the report into the params
     params[:sql] = @report.custom_sql unless @report.custom_sql.blank?
-    # get the report data    
+    # get the report data
     @data = report_instance.get_data(@organization_list, params)
 
     respond_to do |format|
-      format.js 
+      format.js
       format.json { render :json => @data.to_json }
     end
-    
+
   end
-  
-  # renders a dashboard detail page. Actual details depends on the id parameter passed
-  # from the view
+
   def show
-    
+
     @report_filter_type = params[:report_filter_type]
     @asset_types = []
     AssetType.all.each do |at|
@@ -53,31 +49,30 @@ class ReportsController < OrganizationAwareController
 
     if @report
       @report_view = @report.view_name
-      add_breadcrumb @report.name, report_path(@report)
-      @page_title = @report.name
-      
+      add_breadcrumb @report.name
+
       report_instance = @report.class_name.constantize.new
       # inject the sql for the report into the params
       params[:sql] = @report.custom_sql unless @report.custom_sql.blank?
       # get the report data
       @data = report_instance.get_data(@organization_list, params)
-      
+
       respond_to do |format|
         format.html
       end
     end
   end
-  
+
   #------------------------------------------------------------------------------
   #
   # Private Methods
   #
   #------------------------------------------------------------------------------
   private
-      
+
   # Returns the selected report
   def get_report
-    # load this report and create the report instance 
+    # load this report and create the report instance
     report = Report.find(params[:id])
     # check that the current use is authorized to view the report
     unless report.nil?
