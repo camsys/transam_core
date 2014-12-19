ac4b1af252b7fc1a70079cb36e10bf16cdf34552
Julian Ray <jray@camsys.com>
2014-12-19 10:09:48 -0500

    Fix up the asset disposition service to limit to a specific organization and set the default fiscal year to the current planning year.

--- app/services/asset_disposition_service.rb
+++ app/services/asset_disposition_service.rb
@@ -1,7 +1,8 @@
 #------------------------------------------------------------------------------
 #
-# AssetService
+# AssetDispositionService
 #
+# Contains business logic associated with managing the disposition of assets
 #
 #
 #------------------------------------------------------------------------------
@@ -15,21 +16,32 @@ class AssetDispositionService
   #
   #------------------------------------------------------------------------------
 
-  def disposition_list(fiscal_year=nil, asset_type_id=nil, asset_subtype_id=nil)
+  def disposition_list(org, fiscal_year=current_planning_year_year, asset_type_id=nil, asset_subtype_id=nil)
+
+    Rails.logger.debug "AssetDispositionService:  disposition_list()"
+    #
+    if org.nil?
+      Rails.logger.warn "AssetDispositionService:  disposition list: Org ID cannot be null"
+      return []
+    end
+
     # Start to set up the query
     conditions  = []
     values      = []
 
+    # Filter for the selected org
+    conditions << "organization_id = ?"
+    values << org.id
+
+    # Can't already be marked as disposed
     conditions << "disposition_date IS NOT NULL"
 
-    if fiscal_year.nil?
-      conditions << "scheduled_replacement_year >= ?"
-      values << current_fiscal_year_year
-    else
-      conditions << "scheduled_replacement_year = ?"
-      values << fiscal_year
-    end
+    # Scheduled replacement year, defaults to the next planning year unless
+    # specified
+    conditions << "scheduled_replacement_year >= ?"
+    values << fiscal_year
 
+    # Limit by asset type
     unless asset_type_id.nil?
       conditions << "asset_type_id = ?"
       values << asset_type_id
