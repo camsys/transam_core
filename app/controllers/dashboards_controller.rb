de0c14cdfe6615eb10c11064ddda7287c2600df3
Alex Bromley <evilbikes79@gmail.com>
2014-12-19 13:49:27 -0500

    using dashboard for pilot.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,11 @@ class DashboardsController < OrganizationAwareController
    add_breadcrumb "Home", :root_path
        
   def index
-                    
+  	if params["search_text"].present?
+  		search_text = params["search_text"]
+		@searched_assets = Asset.search(search_text).records
+		@searched_customers = Customer.search(search_text).records
+	end
   end
 
 end
