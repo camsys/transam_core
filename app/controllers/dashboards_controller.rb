c86486c76bc386c76533b110e9bde11a2b05a5cf
Alex Bromley <evilbikes79@gmail.com>
2014-12-22 16:24:25 -0500

    fleshing out a dashboard test [#84689140]

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,12 @@ class DashboardsController < OrganizationAwareController
    add_breadcrumb "Home", :root_path
        
   def index
-                    
+  	if params["search_text"]
+  		client = Swiftype::Client.new
+		@dashboard_search_results = client.search("engine", params["search_text"])
+		binding.pry
+		x = 1
+	end
   end
 
 end
