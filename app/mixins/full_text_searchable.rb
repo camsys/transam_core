04bce494d03215aa2d5ace21d3c9d63cc697783f
Alex Bromley <evilbikes79@gmail.com>
2015-01-07 10:34:36 -0500

    remove method probably not going to use

--- app/mixins/full_text_searchable.rb
+++ app/mixins/full_text_searchable.rb
@@ -8,25 +8,19 @@
 #------------------------------------------------------------------------------
 module FullTextSearchable
 
-  def write_to_full_text_search_index(object_key)
+  def write_to_full_text_search_index
 
     text_blob = ""
     separator = " "
     searchable_fields.each { |searchable_field|
-      text_blob += self[searchable_field].to_s
+      text_blob += self.send(searchable_field).to_s
       text_blob += separator
     }
 
     FullTextSearchIndex.find_or_create_by(object_key: object_key) do |full_text_search_index|
   		full_text_search_index.search_text = text_blob
-	end
+	  end
     
-  end
-
-  def search_myself_for_answers(search_text)
-
-  	FullTextSearchIndex.find_by(search_text: search_text)
-
   end
 
 end
