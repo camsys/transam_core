518b02e4d9eeea46699bccd40ed4754ea75ba59d
Julian Ray <jray@camsys.com>
2015-01-09 11:31:33 -0500

    Refactoring FormAware controllers

--- app/controllers/form_aware_controller.rb
+++ app/controllers/form_aware_controller.rb
@@ -6,6 +6,31 @@ class FormAwareController < OrganizationAwareController
   # set the @form_class variable before any actions are invoked
   before_action :get_form_class
 
+
+  # Used by all form controllers to update the form status as it goes thorugh
+  # its workflows
+  def fire_workflow_event
+
+    # Check that this is a valid event name for the state machines
+    if @form.class.event_names.include? params[:event]
+      event_name = params[:event]
+      if @form.fire_state_event(event_name)
+        event = WorkflowEvent.new
+        event.creator = current_user
+        event.accountable = @form
+        event.event_type = event_name
+        event.save
+      else
+        notify_user(:alert, "Could not #{event_name.humanize} form #{@form}")
+      end
+    else
+      notify_user(:alert, "#{params[:event_name]} is not a valid event for a #{@form.class.name}")
+    end
+
+    redirect_to :back
+
+  end
+
   protected
 
   # Returns the selected form
