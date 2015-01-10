55556d41132ab765adc3c85eae8d26c3f9cd704e
Julian Ray <jray@camsys.com>
2015-01-10 09:50:33 -0500

    Fix phone number migration to add extension only wherever there is a phone number field

--- db/migrate/20150109204655_fix_phone_number_fields.rb
+++ db/migrate/20150109204655_fix_phone_number_fields.rb
@@ -1,12 +1,7 @@
 class FixPhoneNumberFields < ActiveRecord::Migration
   def change
-    Organization.all.each do |o|
-      old_phone = o.phone
-      new_phone = old_phone.gsub(/[^0-9]/, "")
-      o.update(phone: new_phone)
-    end
-    puts Organization.pluck(:phone)
-    change_column :organizations, :phone, :string, :limit => 10, :null => false
     add_column    :organizations, :phone_ext, :string, :limit => 6, :default => nil, :after => :phone
+    add_column    :users,         :phone_ext, :string, :limit => 6, :default => nil, :after => :phone
+    add_column    :vendors,       :phone_ext, :string, :limit => 6, :default => nil, :after => :phone
   end
 end
