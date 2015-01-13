f26d7f2e483e81cc2282dba323d78e916e37a42a
Alex Bromley <evilbikes79@gmail.com>
2015-01-12 20:20:54 -0500

    validate field presence [#84689140]

--- app/models/keyword_search_index.rb
+++ app/models/keyword_search_index.rb
@@ -1,5 +1,9 @@
 class KeywordSearchIndex < ActiveRecord::Base
 
+	validates     :object_key,	:presence => true
+  validates     :object_class,	:presence => true
+  validates     :search_text,	:presence => true
+
 	def to_s
 		self.name.to_s
 	end
