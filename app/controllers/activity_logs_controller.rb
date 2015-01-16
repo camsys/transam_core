b53c1211a1cbec6ff3935c544b70792f293653b5
Aaron Magil <amagil@camsys.com>
2015-01-16 11:20:28 -0500

    Allow access to activity logs for entire org list, not just own org

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -5,7 +5,7 @@ class ActivityLogsController < OrganizationAwareController
     @page_title = 'Activity Log'
    
     # get the policies for this agency 
-    @activities = ActivityLog.where('organization_id = ?', @organization.id).order('activity_time')
+    @activities = ActivityLog.where(organization: @organization_list).order('activity_time')
     
     respond_to do |format|
       format.html # index.html.erb
