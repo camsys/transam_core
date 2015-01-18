d2dbcb4d15f3b8497a9ed7b04ef4db0be2829412
Aaron Magil <amagil@camsys.com>
2015-01-17 20:06:02 -0500

    Revert "Give Process Log access to the url_helpers so it can create direct links.  Known and intended violation of MVC" The url_helpers need to live in the caller, not the process log.
    This reverts commit 963aa4d10e52f3606495c1a1044d9864971121a0.

--- app/models/process_log.rb
+++ app/models/process_log.rb
@@ -7,7 +7,6 @@
 #
 #------------------------------------------------------------------------------
 class ProcessLog
-  include Rails.application.routes.url_helpers # Known violation of MVC.  Do not change willy-nilly
 
   attr_reader :process_log
 
