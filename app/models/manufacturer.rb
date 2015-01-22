172f16b868318a91a5085fb658a603ff71035791
Aaron Magil <amagil@camsys.com>
2015-01-22 18:27:50 -0500

    Build search method on Manufacturer

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -3,6 +3,19 @@ class Manufacturer < ActiveRecord::Base
   # default scope
   default_scope { where(:active => true).order('code') }
 
+  def self.search(text, exact = true, **params) # **params is a ruby parameter "hash-splat"
+    if exact
+      x = where('name = ? OR code = ?', text, text)
+      x = x.where(params) if params
+    else
+      val = "%#{text.upcase}%"
+      x = where('UPPER(name) LIKE ? OR UPPER(code) LIKE ?', val, val)
+      x = x.where(params) if params
+    end
+    x.first
+  end
+
+
   def asset_count(org)
     Asset.where(:organization_id => org.id, :manufacturer_id => id).count
   end
