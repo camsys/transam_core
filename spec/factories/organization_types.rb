3532a04c08ff22fd1cf9a5eb5312fa979d330908
Julian Ray <jray@camsys.com>
2015-02-04 08:38:42 -0500

    remove travelcons

--- spec/factories/organization_types.rb
+++ spec/factories/organization_types.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :organization_type do
     name 'TestOrg'
     class_name 'TestOrg'
-    display_icon_name 'fa travelcon-bus'
+    display_icon_name 'fa fa-bus'
     map_icon_name 'greenIcon'
     description 'Organizations who own, operate, or manage transit assets.'
     active 1
