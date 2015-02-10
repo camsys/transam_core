bda11bb442a7806e2fedd92547e670c60e5df904
Alex Bromley <evilbikes79@gmail.com>
2015-02-10 00:15:09 -0500

    should work but need to figure out how to test properly[#88004786]

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,7 +3,7 @@ class ErrorsController < TransamController
   layout "errored"
   
   def show
-    render status_code.to_s, :status => status_code
+  	render(:file => File.join(Rails.root, 'app/views/errors/#{status_code}.html'), :status => status_code)
   end
  
   protected
