e204bafe1ce6debb45b57ce422510a896025e3f2
Aaron Magil <amagil@camsys.com>
2015-02-27 11:52:47 -0500

    Fix NPE on location update event

--- app/models/location_update_event.rb
+++ app/models/location_update_event.rb
@@ -55,7 +55,7 @@ class LocationUpdateEvent < AssetEvent
     self.parent = Asset.find_by_object_key(object_key)
   end
   def parent_key
-    parent.object_key
+    parent.object_key if parent
   end
 
   protected
