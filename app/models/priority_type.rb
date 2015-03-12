fa0906c1ed5db14fb42c4b65006dad5e7b0e89e9
Julian Ray <jray@camsys.com>
2015-03-12 08:05:24 -0400

    Tweak on PriorityType model

--- app/models/priority_type.rb
+++ app/models/priority_type.rb
@@ -1,13 +1,13 @@
 class PriorityType < ActiveRecord::Base
-            
+
   #attr_accessible :name, :description, :active, :default
-        
+
   # default scope
   default_scope { where(:active => true) }
-  
+
   def self.default
-    where(:is_default => true).first
-  end      
+    find_by(:is_default => true)
+  end
 
   def to_s
     name
