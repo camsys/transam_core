06ae8b6bba43b818029303544ed7eb2ff5e877cf
Julian Ray <jray@camsys.com>
2015-03-27 07:12:34 -0400

    Fix typo in seed file and rename menu item for rehabs

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ asset_event_types = [
   {:active => 1, :name => 'Schedule disposition',       :display_icon_name => "fa fa-times-circle",       :description => 'Scheduled disposition',       :class_name => 'ScheduleDispositionUpdateEvent',      :job_name => 'AssetScheduleDispositionUpdateJob'},
   {:active => 1, :name => 'Update the service status',  :display_icon_name => "fa fa-bell",  :description => 'Service Status Update',  :class_name => 'ServiceStatusUpdateEvent',  :job_name => 'AssetServiceStatusUpdateJob'},
   {:active => 1, :name => 'Update the location',       :display_icon_name => "fa fa-map-marker",       :description => 'Location Update',       :class_name => 'LocationUpdateEvent',      :job_name => 'AssetLocationUpdateJob'},
-  {:active => 1, :name => 'Add a rehabilitation',       :display_icon_name => "fa fa-wrench",       :description => 'Rehabilitation Update',       :class_name => 'RehabilitationUpdateEvent',      :job_name => 'AssetRehabilitationUpdateJob'},
+  {:active => 1, :name => 'Record a rebuild/rehabilitation',       :display_icon_name => "fa fa-wrench",       :description => 'Rehabilitation Update',       :class_name => 'RehabilitationUpdateEvent',      :job_name => 'AssetRehabilitationUpdateJob'},
   {:active => 1, :name => 'Record final disposition',     :display_icon_name => "fa fa-ban",      :description => 'Disposition Update',     :class_name => 'DispositionUpdateEvent',    :job_name => 'AssetDispositionUpdateJob'}
 ]
 
@@ -202,6 +202,6 @@ data.each do |row|
   x.save!
 end
 
-subsytems = [
+subsystems = [
   {:active => 1, :name => "Other"}
 ]
