e21cf4b2780047ce51e604370cdd641bcda77b24
Julian Ray <jray@camsys.com>
2015-03-27 07:12:13 -0400

    Remove default scope for report model and add an active scope instead to allow RailsAdmin to load inactive reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,22 +2,22 @@ class Report < ActiveRecord::Base
 
   # associations
   belongs_to :report_type
-  
-  #attr_accessible :active, :report_type_id, :name, :class_name, :view_name, :description, :show_in_nav
-        
+
+
   # default scope
-  default_scope { where(:active => true) }
-      
-  scope :show_in_nav, -> { where(:show_in_nav => true) }
-  scope :show_in_dashboard, -> { where(:show_in_dashboard => true) }
+  #default_scope { where(:active => true) }
+
+  scope :active, -> { where(:active => true) }
+  scope :show_in_nav, -> { where(:active => true, :show_in_nav => true) }
+  scope :show_in_dashboard, -> { where(:active => true, :show_in_dashboard => true) }
 
   def to_s
     name
   end
-  
-  # Return an array of role anmes allowed to access this report
+
+  # Return an array of role names allowed to access this report
   def role_names
     roles.split(',')
   end
-  
+
 end
