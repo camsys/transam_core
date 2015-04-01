ec3f3a09091555f7ef694cd185faeb97f2401607
Julian Ray <jray@camsys.com>
2015-04-01 08:57:53 -0400

    Whitespace cleanup

--- app/services/policy_rule_service.rb
+++ app/services/policy_rule_service.rb
@@ -4,21 +4,21 @@
 #
 # Attempts to find the policy rule that best matches an asset
 #
-# The service returns a single policy rule or nil if no matching policy rule is 
-# found. This is a naieve version that simply returns the first match based on the 
+# The service returns a single policy rule or nil if no matching policy rule is
+# found. This is a naieve version that simply returns the first match based on the
 # asset subtype. Applicaitons should override this implementation with more
 # specific logic
 #
 #------------------------------------------------------------------------------
 class PolicyRuleService
-    
+
   #------------------------------------------------------------------------------
   #
   # Match
   #
-  # Single entry point. User passes in a policy and an asset. 
+  # Single entry point. User passes in a policy and an asset.
   #
-  #------------------------------------------------------------------------------  
+  #------------------------------------------------------------------------------
   def match(policy, asset)
 
     if policy.nil?
@@ -29,11 +29,11 @@ class PolicyRuleService
       Rails.logger.info "asset cannot be nil."
       return
     end
-    
+
     p = policy
     # make sure the asset is typed
     a = asset.is_typed? ? asset : Asset.get_typed_asset(asset)
-    
+
     # Check the rules for this policy and its parents
     p = policy
     while p
@@ -54,17 +54,17 @@ class PolicyRuleService
   #
   #------------------------------------------------------------------------------
   protected
-  
+
   # Perform the actual logic for matching a policy item to an asset.
-  def evaluate(policy, asset)   
+  def evaluate(policy, asset)
     policy.policy_items.where('asset_subtype_id = ?', asset.asset_subtype_id).first
   end
-  
+
   #------------------------------------------------------------------------------
   #
   # Private Methods
   #
   #------------------------------------------------------------------------------
   private
-  
+
 end
