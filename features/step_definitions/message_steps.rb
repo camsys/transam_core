36fef859270bf4f3f21f0f9444dc7c6d7477e5d4
Lydia Chang <mathmerized@gmail.com>
2015-04-10 09:23:01 -0400

    fix cucumber tests for messages for changes

--- features/step_definitions/message_steps.rb
+++ features/step_definitions/message_steps.rb
@@ -3,7 +3,6 @@ Given(/^a \[message\] exists for "(.*?)"$/) do |email|
   FactoryGirl.create(:message, :to_user => User.find_by(:email => email))
 end
 
-### WHEN ###
-When(/^I am at the "New Message" page$/) do
-  visit new_user_message_path(User.first)
+When(/^I am at My Messages page for "(.*?)"$/) do |email|
+  visit user_messages_path(User.find_by(:email => email))
 end
