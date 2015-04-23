0837205dea93a7d45a50ce6256f0b3107f818fce
Julian Ray <jray@camsys.com>
2015-04-23 14:11:43 -0400

    Clean up dashboard tool renderer

--- app/controllers/transam_controller.rb
+++ app/controllers/transam_controller.rb
@@ -1,33 +1,33 @@
 class TransamController < ApplicationController
-  
+
   before_filter :authenticate_user!
-  before_filter :set_timezone 
- 
+  before_filter :set_timezone
+
   # Include the rails4 style form parameters mixin
   include TransamAttributes
 
   # determine which layout to use based on the current user state
   layout :layout_by_resource
-      
+
   OBJECT_CACHE_EXPIRE_SECONDS = Rails.application.config.object_cache_expire_seconds
-           
+
   # Enumerables for view types for index views
   VIEW_TYPE_LIST  = 1   # thumbnails
   VIEW_TYPE_TABLE = 2   # table
   VIEW_TYPE_MAP   = 3   # map
-       
+
   # A set of utilities to determine the database adapter type
   def is_mysql
     get_db_adapter == 'mysql2'
   end
   def is_postgresql
-    get_db_adapter == 'postgres'    
+    get_db_adapter == 'postgres'
   end
   # Returns the name of the database adapter
   def get_db_adapter
-    ActiveRecord::Base.configurations[Rails.env]['adapter'] 
+    ActiveRecord::Base.configurations[Rails.env]['adapter']
   end
-  
+
   # Centralized message sender that can be overriden by an implementation
   def notify_user(type, message)
     # if there is a notify_user method in ApplicationController use it otherwise
@@ -38,7 +38,7 @@ class TransamController < ApplicationController
       flash[type] = message
     end
   end
-  
+
   # Determine which layout to use based on the authorized state
   def layout_by_resource
     if user_signed_in?
@@ -46,7 +46,7 @@ class TransamController < ApplicationController
     else
       "unauthorized"
     end
-  end       
+  end
     
   protected
 
@@ -60,7 +60,7 @@ class TransamController < ApplicationController
     end
     cache_objects(cache_key, list)
   end
-  
+
   # Sets view vars @prev_record_key, @next_record_key, @total_rows and @row_number for
   # a current object
   def get_next_and_prev_object_keys(obj, cache_key)
@@ -72,7 +72,7 @@ class TransamController < ApplicationController
     # make sure we have a list and an object to find
     if id_list && obj
       @total_rows = id_list.size
-      # get the index of the current object in the array      
+      # get the index of the current object in the array
       current_index = id_list.index(obj.object_key)
       if current_index
         @row_number = current_index + 1
@@ -100,10 +100,10 @@ class TransamController < ApplicationController
       val = "%#{search_text}"
     else # contains or any
       val = "%#{search_text}%"
-    end  
-    val  
+    end
+    val
   end
-  
+
   # Queues a job to be executed in the background
   def fire_background_job(job, priority = 0)
     Delayed::Job.enqueue job, :priority => priority
@@ -114,7 +114,7 @@ class TransamController < ApplicationController
     Rails.logger.debug "ApplicationController CACHE put for key #{get_cache_key(current_user, key)}"
     Rails.cache.write(get_cache_key(current_user, key), objects, :expires_in => expires_in)
   end
-  
+
   # Return an array of cached objects
   def get_cached_objects(key)
     Rails.logger.debug "ApplicationController CACHE get for key #{get_cache_key(current_user, key)}"
@@ -126,7 +126,7 @@ class TransamController < ApplicationController
     Rails.logger.debug "ApplicationController CACHE clear for key #{get_cache_key(current_user, key)}"
     Rails.cache.delete(get_cache_key(current_user, key))
   end
-    
+
   # generates a cache key that is unique for a user and key name
   def get_cache_key(user, key)
     return "%06d:%s" % [user.id, key]
@@ -140,15 +140,15 @@ class TransamController < ApplicationController
       view_type = VIEW_TYPE_LIST
     end
     # remember the view type in the session
-    session[session_var] = view_type   
-    return view_type 
+    session[session_var] = view_type
+    return view_type
   end
-    
+
   #
   # Set the timezone for the session. If the user has one set in the database it is used
   # otherwise one is defutled
   def set_timezone
-    Time.zone = current_user.nil? ? 'Eastern Time (US & Canada)' : current_user.timezone 
-  end 
-  
+    Time.zone = current_user.nil? ? 'Eastern Time (US & Canada)' : current_user.timezone
+  end
+
 end
