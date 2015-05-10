5ead7c6ba978ad4dc18bfc678d93407ee1f257fd
Julian Ray <jray@camsys.com>
2015-05-10 16:49:38 -0400

    Move FuelType as a core lookup

---
+++ app/models/fuel_type.rb
@@ -0,0 +1,22 @@
+class FuelType < ActiveRecord::Base
+
+  #attr_accessible :name, :description, :code, :active
+
+  # default scope
+  default_scope { where(:active => true) }
+
+  def self.search(text, exact = true)
+    if exact
+      x = where('name = ? OR code = ? OR description = ?', text, text, text).first
+    else
+      val = "%#{text}%"
+      x = where('name = LIKE ? OR code LIKE ? OR description LIKE ?', val, val, val).first
+    end
+    x
+  end
+
+  def to_s
+    name
+  end
+
+end
