4378dfd7018af813412f1ba77bd4a61817e48805
Julian Ray <jray@camsys.com>
2015-05-20 08:39:49 -0400

    Allow assets to preserve their object key

--- lib/transam_core/transam_object_key.rb
+++ lib/transam_core/transam_object_key.rb
@@ -13,17 +13,21 @@ module TransamObjectKey
     before_validation(:on => :create) do
       generate_object_key(:object_key)
     end
-    
+
     validates :object_key, :presence => true, :uniqueness => true, :length => { is: 12 }
   end
 
+  # Only generate an objecrt key if one is not set. This allows objects to be
+  # replaced but maintain the same object key
   def generate_object_key(column)
-    begin
-      # Note Time.now.to_f converts to seconds since Epoch, which is correct
-      self[column] = (Time.now.to_f * 10000000).to_i.to_s(24).upcase
-    end #while self.exists?(column => self[column])
+    if self[column].blank?
+      begin
+        # Note Time.now.to_f converts to seconds since Epoch, which is correct
+        self[column] = (Time.now.to_f * 10000000).to_i.to_s(24).upcase
+      end #while self.exists?(column => self[column])
+    end
   end
-  
+  # Use the object key as the restful parameter
   def to_param
     object_key
   end
