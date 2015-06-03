21b0b48ff8da76cd4227cbd6d058d3ed1b0c22cf
Sathyan1983 <Sathyan.Mathai@gmail.com>
2015-06-02 22:39:27 -0400

    Changed asset_searcher_spec to perform searches before, after, or during a particular year.

--- spec/searches/asset_search_spec.rb
+++ spec/searches/asset_search_spec.rb
@@ -7,12 +7,30 @@ RSpec.describe AssetSearch, :type => :model do
   # Instance Methods
   #
   #------------------------------------------------------------------------------
-  it 'should be able to search by in service date' do
+  it 'should be able to search for assets put in service in a particular year' do
     searcher = AssetSearcher.new();
     expect(searcher.attributes).to include(:in_service_date)
     expect(searcher.attributes).to include(:in_service_date_comparator)
 
     searcher = AssetSearcher.new(:in_service_date => 2014, :in_service_date_comparator => '0')
     expect(searcher.data). to eq(Asset.where(‘organization_id = 1’))
+  end
+
+  it 'should be able to search for assets put in service in after a particular year' do
+    searcher = AssetSearcher.new();
+    expect(searcher.attributes).to include(:in_service_date)
+    expect(searcher.attributes).to include(:in_service_date_comparator)
+
+    searcher = AssetSearcher.new(:in_service_date => 2014, :in_service_date_comparator => '1')
+    expect(searcher.data). to eq(Asset.where(‘organization_id > 1’))
+  end
+
+  it 'should be able to search for assets put in service in before a particular year' do
+    searcher = AssetSearcher.new();
+    expect(searcher.attributes).to include(:in_service_date)
+    expect(searcher.attributes).to include(:in_service_date_comparator)
+
+    searcher = AssetSearcher.new(:in_service_date => 2014, :in_service_date_comparator => '-1')
+    expect(searcher.data). to eq(Asset.where(‘organization_id < 1’))
   end
 end
