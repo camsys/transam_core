4544272cb58140bdf9114448a5fe0af9640a0b32
Sathyan1983 <Sathyan.Mathai@gmail.com>
2015-06-03 08:26:50 -0400

    added book value search testing.

--- spec/searches/asset_search_spec.rb
+++ spec/searches/asset_search_spec.rb
@@ -33,4 +33,31 @@ RSpec.describe AssetSearch, :type => :model do
     searcher = AssetSearcher.new(:in_service_date => 2014, :in_service_date_comparator => '-1')
     expect(searcher.data). to eq(Asset.where('in_service_year < 2014'))
   end
+
+  it 'should be able to search for assets equal to a particular book value' do
+    searcher = AssetSearcher.new();
+    expect(searcher.attributes).to include(:book_value)
+    expect(searcher.attributes).to include(:book_value_comparator)
+
+    searcher = AssetSearcher.new(:book_value => 30000, :book_value_comparator => '0')
+    expect(searcher.data). to eq(Asset.where('book_value = 30000'))
+  end
+
+  it 'should be able to search for assets greater than a particular book value' do
+    searcher = AssetSearcher.new();
+    expect(searcher.attributes).to include(:book_value)
+    expect(searcher.attributes).to include(:book_value_comparator)
+
+    searcher = AssetSearcher.new(:book_value => 30000, :book_value_comparator => '1')
+    expect(searcher.data). to eq(Asset.where('book_value > 30000'))
+  end
+
+  it 'should be able to search for assets less than a particular book value' do
+    searcher = AssetSearcher.new();
+    expect(searcher.attributes).to include(:book_value)
+    expect(searcher.attributes).to include(:book_value_comparator)
+
+    searcher = AssetSearcher.new(:book_value => 30000, :book_value_comparator => '-1')
+    expect(searcher.data). to eq(Asset.where('book_value < 30000'))
+  end
 end
