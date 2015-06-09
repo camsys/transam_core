c55bff15d973d4fa558ff9c6d6234d6d165a6507
Sathyan1983 <Sathyan.Mathai@gmail.com>
2015-06-09 11:56:47 -0400

    Edited remaining tests to rely on the asset factory.

--- spec/searches/asset_search_spec.rb
+++ spec/searches/asset_search_spec.rb
@@ -46,87 +46,108 @@ RSpec.describe AssetSearcher do
   #
   #------------------------------------------------------------------------------
   it 'should be able to search by purchase cost' do
-    searcher = AssetSearcher.new(:purchase_cost => [bus.purchase_cost], :purchase_cost_comparator => '0', :organization_id => bus.organization.id )
+    asset = create(:asset, :purchase_cost => 10000, :organization_id => 1)
+    lesser = create(:asset, :purchase_cost => 2000, :organization_id => 1)
+
+    searcher = AssetSearcher.new(:purchase_cost => [asset.purchase_cost], :purchase_cost_comparator => '0', :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:purchase_cost)).to be true
-    expect(searcher.data).to eq(Asset.where('purchase_cost = ?', [bus.purchase_cost]))
+    expect(searcher.data).to eq(Asset.where('purchase_cost = ?', [asset.purchase_cost]))
 
-    searcher = AssetSearcher.new(:purchase_cost => [bus.purchase_cost], :purchase_cost_comparator => '-1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('purchase_cost < ?', [bus.purchase_cost]))
+    searcher = AssetSearcher.new(:purchase_cost => [asset.purchase_cost], :purchase_cost_comparator => '-1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('purchase_cost < ?', [asset.purchase_cost]))
 
-    searcher = AssetSearcher.new(:purchase_cost => [bus.purchase_cost], :purchase_cost_comparator => '1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('purchase_cost > ?', [bus.purchase_cost]))
+    searcher = AssetSearcher.new(:purchase_cost => [asset.purchase_cost], :purchase_cost_comparator => '1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('purchase_cost > ?', [asset.purchase_cost]))
   end
 
   it 'should be able to search by replacement year' do
-    searcher = AssetSearcher.new(:replacement_year => bus.replacement_year, :replacement_year_comparator => '0', :organization_id => bus.organization.id )
+    asset = create(:asset, :replacement_year => 2020, :organization_id => 1)
+    lesser = create(:asset, :replacement_year => 2010, :organization_id => 1)
+
+    searcher = AssetSearcher.new(:replacement_year => asset.replacement_year, :replacement_year_comparator => '0', :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:replacement_year)).to be true
-    expect(searcher.data).to eq(Asset.where('replacement_year = ?', bus.replacement_year))
+    expect(searcher.data).to eq(Asset.where('replacement_year = ?', asset.replacement_year))
 
-    searcher = AssetSearcher.new(:replacement_year => bus.replacement_year, :replacement_year_comparator => '-1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('replacement_year < ?', bus.replacement_year))
+    searcher = AssetSearcher.new(:replacement_year => asset.replacement_year, :replacement_year_comparator => '-1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('replacement_year < ?', asset.replacement_year))
 
-    searcher = AssetSearcher.new(:replacement_year => bus.replacement_year, :replacement_year_comparator => '1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('replacement_year > ?', [bus.replacement_year]))
+    searcher = AssetSearcher.new(:replacement_year => asset.replacement_year, :replacement_year_comparator => '1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('replacement_year > ?', [asset.replacement_year]))
   end
 
   it 'should be able to search by scheduled replacement year' do
-    searcher = AssetSearcher.new(:scheduled_replacement_year => bus.scheduled_replacement_year, :scheduled_replacement_year_comparator => '0', :organization_id => bus.organization.id )
+    asset = create(:asset, :scheduled_replacement_year => 2020, :organization_id => 1)
+    lesser = create(:asset, :scheduled_replacement_year => 2010, :organization_id => 1)
+
+    searcher = AssetSearcher.new(:scheduled_replacement_year => asset.scheduled_replacement_year, :scheduled_replacement_year_comparator => '0', :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:scheduled_replacement_year)).to be true
-    expect(searcher.data).to eq(Asset.where('scheduled_replacement_year = ?', bus.scheduled_replacement_year))
+    expect(searcher.data).to eq(Asset.where('scheduled_replacement_year = ?', asset.scheduled_replacement_year))
 
-    searcher = AssetSearcher.new(:scheduled_replacement_year => scheduled_replacement_year, :scheduled_replacement_year_comparator => '-1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('scheduled_replacement_year < ?', bus.scheduled_replacement_year))
+    searcher = AssetSearcher.new(:scheduled_replacement_year => scheduled_replacement_year, :scheduled_replacement_year_comparator => '-1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('scheduled_replacement_year < ?', asset.scheduled_replacement_year))
 
-    searcher = AssetSearcher.new(:scheduled_replacement_year => bus.scheduled_replacement_year, :scheduled_replacement_year_comparator => '1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('scheduled_replacement_year > ?', bus.scheduled_replacement_year))
+    searcher = AssetSearcher.new(:scheduled_replacement_year => asset.scheduled_replacement_year, :scheduled_replacement_year_comparator => '1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('scheduled_replacement_year > ?', asset.scheduled_replacement_year))
   end
 
   it 'should be able to search by policy replacement year' do
-    searcher = AssetSearcher.new(:policy_replacement_year => bus.policy_replacement_year, :policy_replacement_year_comparator => '0', :organization_id => bus.organization.id )
+    asset = create(:asset, :policy_replacement_year => 2020, :organization_id => 1)
+    lesser = create(:asset, :policy_replacement_year => 2010, :organization_id => 1)
+
+    searcher = AssetSearcher.new(:policy_replacement_year => asset.policy_replacement_year, :policy_replacement_year_comparator => '0', :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:policy_replacement_year)).to be true
-    expect(searcher.data).to eq(Asset.where('policy_replacement_year = ?', bus.policy_replacement_year))
+    expect(searcher.data).to eq(Asset.where('policy_replacement_year = ?', asset.policy_replacement_year))
 
-    searcher = AssetSearcher.new(:policy_replacement_year => bus.policy_replacement_year, :policy_replacement_year_comparator => '-1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('policy_replacement_year < ?', bus.policy_replacement_year))
+    searcher = AssetSearcher.new(:policy_replacement_year => asset.policy_replacement_year, :policy_replacement_year_comparator => '-1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('policy_replacement_year < ?', asset.policy_replacement_year))
 
-    searcher = AssetSearcher.new(:policy_replacement_year => bus.policy_replacement_year, :scheduled_replacement_year_comparator => '1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('policy_replacement_year > ?', bus.policy_replacement_year))
+    searcher = AssetSearcher.new(:policy_replacement_year => asset.policy_replacement_year, :scheduled_replacement_year_comparator => '1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('policy_replacement_year > ?', asset.policy_replacement_year))
   end
 
   it 'should be able to search by purchase date' do
-    searcher = AssetSearcher.new(:purchase_date => bus.purchase_date, :purchase_date_comparator => '0', :organization_id => bus.organization.id )
+    asset = create(:asset, :purchase_date => Date.today, :organization_id => 1)
+    lesser = create(:asset, :purchase_date => Date.(2000,1,1), :organization_id => 1)
+
+    searcher = AssetSearcher.new(:purchase_date => asset.purchase_date, :purchase_date_comparator => '0', :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:purchase_date)).to be true
-    expect(searcher.data).to eq(Asset.where('purchase_date = ?', bus.purchase_date))
+    expect(searcher.data).to eq(Asset.where('purchase_date = ?', asset.purchase_date))
 
-    searcher = AssetSearcher.new(:purchase_date => bus.purchase_date, :purchase_date_comparator => '-1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('purchase_date < ?', bus.purchase_date))
+    searcher = AssetSearcher.new(:purchase_date => asset.purchase_date, :purchase_date_comparator => '-1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('purchase_date < ?', asset.purchase_date))
 
-    searcher = AssetSearcher.new(:purchase_date => bus.purchase_date, :purchase_date_comparator => '1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('purchase_date > ?', bus.purchase_date))
+    searcher = AssetSearcher.new(:purchase_date => asset.purchase_date, :purchase_date_comparator => '1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('purchase_date > ?', asset.purchase_date))
   end
 
   it 'should be able to search by manufacture year' do
-    searcher = AssetSearcher.new(:manufacture_year => bus.manufacture_year, :manufacture_year_comparator => '0', :organization_id => bus.organization.id )
+    asset = create(:asset, :manufacture_year => Date.today, :organization_id => 1)
+    lesser = create(:asset, :manufacture_year => Date.new(2000,1,1), :organization_id => 1)
+
+    searcher = AssetSearcher.new(:manufacture_year => asset.manufacture_year, :manufacture_year_comparator => '0', :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:manufacture_year)).to be true
-    expect(searcher.data).to eq(Asset.where('manufacture_year = ?', bus.manufacture_year))
+    expect(searcher.data).to eq(Asset.where('manufacture_year = ?', asset.manufacture_year))
 
-    searcher = AssetSearcher.new(:manufacture_year => bus.manufacture_year, :manufacture_year_comparator => '-1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('manufacture_year < ?', bus.manufacture_year))
+    searcher = AssetSearcher.new(:manufacture_year => asset.manufacture_year, :manufacture_year_comparator => '-1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('manufacture_year < ?', asset.manufacture_year))
 
-    searcher = AssetSearcher.new(:manufacture_year => bus.manufacture_year, :manufacture_year_comparator => '1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('manufacture_year > ?', bus.manufacture_year))
+    searcher = AssetSearcher.new(:manufacture_year => asset.manufacture_year, :manufacture_year_comparator => '1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('manufacture_year > ?', asset.manufacture_year))
   end
 
   it 'should be able to search by in service date' do
-    searcher = AssetSearcher.new(:in_service_date => bus.in_service_date, :in_service_date_comparator => '0', :organization_id => bus.organization.id )
+    asset = create(:asset, :in_service_date => Date.today, :organization_id => 1)
+    lesser = create(:asset, :in_service_date => Date.new(2000,1,1), :organization_id => 1)
+
+    searcher = AssetSearcher.new(:in_service_date => asset.in_service_date, :in_service_date_comparator => '0', :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:in_service_date)).to be true
-    expect(searcher.data).to eq(Asset.where('in_service_date = ?', bus.in_service_date))
+    expect(searcher.data).to eq(Asset.where('in_service_date = ?', asset.in_service_date))
 
-    searcher = AssetSearcher.new(:in_service_date => bus.in_service_date, :in_service_date_comparator => '-1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('in_service_date < ?', bus.in_service_date))
+    searcher = AssetSearcher.new(:in_service_date => asset.in_service_date, :in_service_date_comparator => '-1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('in_service_date < ?', asset.in_service_date))
 
-    searcher = AssetSearcher.new(:in_service_date => bus.in_service_date, :in_service_date_comparator => '1', :organization_id => bus.organization.id )
-    expect(searcher.data).to eq(Asset.where('in_service_date > ?', bus.in_service_date))
+    searcher = AssetSearcher.new(:in_service_date => asset.in_service_date, :in_service_date_comparator => '1', :organization_id => asset.organization_id )
+    expect(searcher.data).to eq(Asset.where('in_service_date > ?', asset.in_service_date))
   end
 
   #------------------------------------------------------------------------------
@@ -136,17 +157,21 @@ RSpec.describe AssetSearcher do
   #------------------------------------------------------------------------------
 
   it 'should be able to search by in backlog status' do
-    searcher = AssetSearcher.new(:in_backlog => bus.in_backlog, :organization_id => bus.organization.id )
+    asset = create(:asset, :in_backlog => true, :organization_id => 1)
+
+    searcher = AssetSearcher.new(:in_backlog => asset.in_backlog, :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:in_backlog)).to be true
 
-    expect(searcher.data).to eq(Asset.where('in_backlog = ?', bus.in_backlog))
+    expect(searcher.data).to eq(Asset.where('in_backlog = ?', asset.in_backlog))
   end
 
   it 'should be able to search by in purchased new' do
-    searcher = AssetSearcher.new(:purchased_new => bus.purchased_new, :organization_id => bus.organization.id )
+    asset = create(:asset, :purchased_new => true, :organization_id => 1)
+
+    searcher = AssetSearcher.new(:purchased_new => asset.purchased_new, :organization_id => asset.organization_id )
     expect(searcher.respond_to?(:purchased_new)).to be true
 
-    expect(searcher.data).to eq(Asset.where('purchased_new = ?', bus.purchased_new))
+    expect(searcher.data).to eq(Asset.where('purchased_new = ?', asset.purchased_new))
   end
 
 end
