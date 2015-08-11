918c8c64b4410d44a326a3a4e2fa36c99ade4103
Julian Ray <jray@camsys.com>
2015-08-11 05:29:12 -0400

    Bump version

--- app/calculators/rehabilitation_year_calculator.rb
+++ app/calculators/rehabilitation_year_calculator.rb
@@ -15,10 +15,10 @@ class RehabilitationYearCalculator < Calculator
     # The policy rehabilitation year identifies the year of the assets service
     # that a rehabiliation should be performed. If not set, no rehabiliation
     # will be scheduled otherwise find the fiscal year that is x years after the
-    # fy the asset was placed in service
+    # fiscal year the asset was placed in service
     years_into_service = asset.policy.rehabilitation_year
     if years_into_service.to_i > 0
-      # return the in service fiscaly year plus the policy rehab year
+      # return the in service fiscal year plus the policy rehab year
       fiscal_year_year_on_date(asset.in_service_date) + years_into_service
     else
       nil
--- lib/transam_core/version.rb
+++ lib/transam_core/version.rb
@@ -1,3 +1,3 @@
 module TransamCore
-  VERSION = "0.6.38"
+  VERSION = "0.6.39"
 end
