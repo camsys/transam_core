3a29a6164188a924d687418042a4a77e5047d1f1
Julian Ray <jray@camsys.com>
2015-08-11 05:16:07 -0400

    Checking rehab update job

--- app/jobs/asset_rehabilitation_update_job.rb
+++ app/jobs/asset_rehabilitation_update_job.rb
@@ -1,18 +1,18 @@
 #------------------------------------------------------------------------------
 #
-# AssetLocationUpdateJob
+# AssetRehabilitationUpdateJob
 #
-# Updates an assets location
+# Updates an assets rehabilitation ststus
 #
 #------------------------------------------------------------------------------
 class AssetRehabilitationUpdateJob < AbstractAssetUpdateJob
-  
-  def execute_job(asset)     
+
+  def execute_job(asset)
     asset.update_rehabilitation
   end
 
   def prepare
-    Rails.logger.debug "Executing AssetRehabilitationUpdateJob at #{Time.now.to_s} for Asset #{object_key}"    
+    Rails.logger.debug "Executing AssetRehabilitationUpdateJob at #{Time.now.to_s} for Asset #{object_key}"
   end
-  
+
 end
