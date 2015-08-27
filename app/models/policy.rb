43a2fa00649dd1dc111933dd0195368b3085d7b1
Sathyan1983 <Sathyan.Mathai@gmail.com>
2015-08-27 13:50:00 -0400

    Created associations between policies and the calculators. [#102007628]

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -21,6 +21,10 @@ class Policy < ActiveRecord::Base
   # Every policy belongs to an organization
   belongs_to  :organization
 
+  belongs_to :service_life_calculation_type
+
+  belongs_to :cost_calculation_type
+
   # Every policy can have a parent policy
   belongs_to  :parent, :class_name => 'Policy', :foreign_key => :parent_id
 
