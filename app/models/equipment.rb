33d52366f8310be54cd9f2bce15a540dd190cd65
Julian Ray <jray@camsys.com>
2015-09-18 06:29:25 -0400

    Bump version

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,8 +1,5 @@
 class Equipment < Asset
 
-  # Enable auditing of this model type. Only monitor uodate and destroy events
-  #has_paper_trail :on => [:update, :destroy]
-
   # Callbacks
   after_initialize :set_defaults
 
--- lib/transam_core/version.rb
+++ lib/transam_core/version.rb
@@ -1,3 +1,3 @@
 module TransamCore
-  VERSION = "0.6.53"
+  VERSION = "0.7.01"
 end
