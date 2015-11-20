8af8d092e740dfa6d1add57620f12fb0c94bce42
Julian Ray <jray@camsys.com>
2015-11-20 15:06:06 -0500

    Policy update runs an sogr update on all selected assets

--- app/services/asset_update_job_builder.rb
+++ app/services/asset_update_job_builder.rb
@@ -9,7 +9,7 @@
 class AssetUpdateJobBuilder
 
   def build(organization, options)
-    num_to_update = 0
+
     return 0 if options.blank?  # Shortcut out if they pass a blank
 
     # Otherwise, dynamically build a query, based on options
@@ -33,12 +33,10 @@ class AssetUpdateJobBuilder
     unless asset_group_ids.empty?
       org_assets = org_assets.joins(:asset_groups).where(:asset_groups => {:id => asset_group_ids})
     end
+
     # Rip through the organizations assets, creating a job for each type requested
-    org_assets.pluck(:object_key).each do |object_key|
-      Delayed::Job.enqueue AssetUpdateJob.new(object_key), :priority => 10
-      num_to_update += 1
-    end
-    return num_to_update
+    org_assets.each {|x| x.update_sogr}
+    org_assets.count
   end
 
 
