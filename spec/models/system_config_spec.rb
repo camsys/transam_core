041113c8e33b760a96fc14ab6470518855ef2311
Lydia Chang <mathmerized@gmail.com>
2015-11-30 20:24:35 -0500

    upload and system config rspec

---
+++ spec/models/system_config_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+
+RSpec.describe SystemConfig, :type => :model do
+
+  let(:test_config) { SystemConfig.first }
+
+  describe 'validations' do
+    it 'must have a customer' do
+      test_config.customer_id = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a FY start' do
+      test_config.start_of_fiscal_year = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a map title provider' do
+      test_config.map_tile_provider = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a srid' do
+      test_config.srid = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a min lat' do
+      test_config.min_lat = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a min lon' do
+      test_config.min_lon = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a max lat' do
+      test_config.max_lat = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a max lon' do
+      test_config.max_lon = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a search radius' do
+      test_config.search_radius = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a search units' do
+      test_config.search_units = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a geocoder components' do
+      test_config.geocoder_components = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have a geocoder region' do
+      test_config.geocoder_region = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have forecasting years' do
+      test_config.num_forecasting_years = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have reporting years' do
+      test_config.num_reporting_years = nil
+      expect(test_config.valid?).to be false
+    end
+    it 'must have asset base class name' do
+      test_config.asset_base_class_name = nil
+      expect(test_config.valid?).to be false
+    end
+  end
+
+  it '#instance' do
+    expect(SystemConfig.instance).to eq(test_config)
+  end
+  describe '#transam_module_loaded?' do
+    it 'version' do
+      expect(SystemConfig.transam_module_loaded?('core', '>= 0.0.0.0')).to be true
+    end
+    it 'no version' do
+      expect(SystemConfig.transam_module_loaded?('core')).to be true
+    end
+  end
+  it '#transam_modules' do
+    expect(SystemConfig.transam_modules).to eq(Gem::Specification::find_all_by_name("transam_core"))
+  end
+  it '#transam_module_names' do
+    expect(SystemConfig.transam_module_names).to eq(['core'])
+  end
+
+  it '.default_state_code' do
+    pending('TODO')
+    fail
+  end
+  it '.epoch' do
+    expect(test_config.epoch).to eq(Rails.application.config.epoch)
+  end
+  it '.max_rows_returned' do
+    expect(test_config.max_rows_returned).to eq(Rails.application.config.max_rows_returned)
+  end
+  it '.geocoder_bounds' do
+    expect(test_config.geocoder_bounds).to eq([[test_config.min_lat, test_config.min_lon], [test_config.max_lat, test_config.max_lon]])
+  end
+  it '.map_bounds' do
+    expect(test_config.map_bounds).to eq([[test_config.min_lat, test_config.min_lon], [test_config.max_lat, test_config.max_lon]])
+  end
+end
---
+++ spec/models/upload_spec.rb
@@ -0,0 +1,140 @@
+require 'rails_helper'
+
+RSpec.describe Upload, :type => :model do
+
+  let(:test_upload) { create(:upload) }
+
+  describe 'associations' do
+    it 'has a user' do
+      expect(Upload.column_names).to include('user_id')
+    end
+    it 'has an org' do
+      expect(Upload.column_names).to include('organization_id')
+    end
+    it 'has a file status type' do
+      expect(Upload.column_names).to include('file_status_type_id')
+    end
+    it 'has a file content type' do
+      expect(Upload.column_names).to include('file_content_type_id')
+    end
+    it 'has many asset events' do
+      expect(AssetEvent.column_names).to include('upload_id')
+    end
+  end
+
+  describe 'validations' do
+    it 'must have an org' do
+      test_upload.organization = nil
+      expect(test_upload.valid?).to be false
+    end
+    it 'must have a user' do
+      test_upload.user = nil
+      expect(test_upload.valid?).to be false
+    end
+    it 'must have a file status' do
+      test_upload.file_status_type = nil
+      expect(test_upload.valid?).to be false
+    end
+    it 'must have a file content' do
+      test_upload.file_content_type = nil
+      expect(test_upload.valid?).to be false
+    end
+    it 'must have a file' do
+      test_upload = build_stubbed(:upload, :file=> nil)
+      expect(test_upload.valid?).to be false
+    end
+    it 'must have a file name' do
+      test_upload.original_filename = nil
+      expect(test_upload.valid?).to be false
+    end
+  end
+
+  it '#allowable_params' do
+    expect(Upload.allowable_params).to eq([
+      :organization_id,
+      :user_id,
+      :file_status_type_id,
+      :file_content_type_id,
+      :file,
+      :original_filename,
+      :force_update,
+      :num_rows_processed,
+      :num_rows_added,
+      :num_rows_replaced,
+      :num_rows_failed,
+      :processing_log,
+      :processing_completed_at,
+      :processing_started_at
+    ])
+  end
+
+  describe '.processing_time' do
+    it 'not started' do
+      expect(Upload.new.processing_time).to be nil
+    end
+    it 'started not finished' do
+      test_upload.processing_started_at = Time.now
+      test_upload.processing_completed_at = nil
+      expect(test_upload.processing_time).to be nil
+    end
+    it 'started and finished' do
+      test_upload.processing_started_at = Time.now - 10.minutes
+      test_upload.processing_completed_at = Time.now
+      expect(test_upload.processing_time).to eq(test_upload.processing_completed_at - test_upload.processing_started_at)
+    end
+  end
+  describe '.can_resubmit?' do
+    it 'new record' do
+      expect(Upload.new.can_resubmit?).to be false
+    end
+    it 'file status type that cant delete' do
+      expect(test_upload.can_resubmit?).to be false
+    end
+    it 'file status type that can delete' do
+      test_upload.file_status_type_id = 3
+      expect(test_upload.can_resubmit?).to be true
+    end
+  end
+  describe '.can_delete?' do
+    it 'new record' do
+      expect(Upload.new.can_delete?).to be false
+    end
+    it 'file status type that cant delete' do
+      expect(test_upload.can_delete?).to be false
+    end
+    it 'file status type that can delete' do
+      test_upload.file_status_type_id = 3
+      expect(test_upload.can_delete?).to be true
+    end
+  end
+  it '.reset' do
+    test_upload.file_status_type = FileStatusType.find_by_name('In Progress')
+    test_upload.num_rows_processed = 1
+    test_upload.num_rows_added = 1
+    test_upload.num_rows_replaced = 1
+    test_upload.num_rows_failed = 1
+    test_upload.num_rows_skipped = 1
+    test_upload.processing_log = 1
+    test_upload.processing_completed_at = 1
+    test_upload.processing_started_at = 1
+    expect(test_upload.num_rows_processed).not_to be nil
+    expect(test_upload.num_rows_added).not_to be nil
+    expect(test_upload.num_rows_replaced).not_to be nil
+    expect(test_upload.num_rows_failed).not_to be nil
+    expect(test_upload.num_rows_skipped).not_to be nil
+    expect(test_upload.processing_log).not_to be nil
+    test_upload.reset
+    expect(test_upload.num_rows_processed).to be nil
+    expect(test_upload.num_rows_added).to be nil
+    expect(test_upload.num_rows_replaced).to be nil
+    expect(test_upload.num_rows_failed).to be nil
+    expect(test_upload.num_rows_skipped).to be nil
+    expect(test_upload.processing_log).to be nil
+    expect(test_upload.file_status_type).to eq(FileStatusType.find_by_name('Unprocessed'))
+
+  end
+
+  it '.set_defaults' do
+    expect(Upload.new.file_status_type).to eq(FileStatusType.find_by_name('Unprocessed'))
+  end
+end
