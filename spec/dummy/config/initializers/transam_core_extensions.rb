7b534c4c927c1fef08a3dbb3c5a1de7221b62313
Lydia Chang <mathmerized@gmail.com>
2015-12-02 09:05:08 -0500

    update config for test app

--- spec/dummy/config/initializers/transam_core_extensions.rb
+++ spec/dummy/config/initializers/transam_core_extensions.rb
@@ -1,4 +1,5 @@
 
 # Defines services to use
 Rails.application.config.new_user_service = "NewUserService"
+Rails.application.config.user_role_service  = "UserRoleService"
 Rails.application.config.policy_analyzer = "PolicyAnalyzer"
