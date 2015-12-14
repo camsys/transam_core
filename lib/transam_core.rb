47a9091e994b6184d6b9bd0972dcc36c152ddb24
Julian Ray <jray@camsys.com>
2015-12-14 11:03:18 -0500

    Move plugins to concerns folder

--- lib/transam_core.rb
+++ lib/transam_core.rb
@@ -1,8 +1,4 @@
 require "transam_core/engine"
-require "transam_core/transam_object_key"
-require "transam_core/transam_keyword_searchable"
-require "transam_core/transam_numeric_sanitizers"
-require "transam_core/transam_workflow"
 
 module TransamCore
 
