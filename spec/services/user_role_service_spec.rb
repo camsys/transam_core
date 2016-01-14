db3b244188982817d3fac0054e38f6553c002b79
Lydia Chang <mathmerized@gmail.com>
2016-01-14 13:06:18 -0500

    rspec for assignable methods for user role service

--- spec/services/user_role_service_spec.rb
+++ spec/services/user_role_service_spec.rb
@@ -3,24 +3,47 @@ require 'rails_helper'
 RSpec.describe UserRoleService, :type => :service do
 
   let(:test_service) { UserRoleService.new }
+  let(:test_guest) { create(:guest) }
   let(:test_user) { create(:normal_user) }
-  let(:test_manager) { create(:admin) }
+  let(:test_manager) { create(:manager) }
+  let(:test_admin) { create(:admin) }
+  let(:test_tech) { create(:technical_contact) }
+
+  describe '.assignable_roles' do
+    it 'guest' do
+        expect((test_service.assignable_roles test_guest).to_json).to eq(Role.where("name = 'guest' OR name = 'user'").to_json)
+    end
+    it 'user' do
+      expect((test_service.assignable_roles test_user).to_json).to eq(Role.where("name = 'guest' OR name = 'user'").to_json)
+    end
+    it 'manager' do
+      expect((test_service.assignable_roles test_manager).to_json).to eq(Role.roles.to_json)
+    end
+  end
+  describe '.assignable_privileges' do
+    it 'admin' do
+      expect((test_service.assignable_privileges test_admin).to_json).to eq(Role.privileges.to_json)
+    end
+    it 'not admin' do
+      expect((test_service.assignable_privileges test_tech).to_json).to eq(Role.where("name = 'technical_contact'").to_json)
+    end
+  end
 
   describe '.set_roles_and_privileges' do
     it 'add user role' do
-      test_service.set_roles_and_privileges test_user, test_manager, nil, []
+      test_service.set_roles_and_privileges test_user, test_admin, nil, []
       test_user.reload
 
       expect(test_user.roles).to include(Role.find_by(:name => 'user'))
     end
     it 'add other roles' do
-      test_service.set_roles_and_privileges test_user, test_manager, Role.find_by(:name => 'technical_contact').id.to_s, []
+      test_service.set_roles_and_privileges test_user, test_admin, Role.find_by(:name => 'technical_contact').id.to_s, []
       test_user.reload
 
       expect(test_user.roles).to include(Role.find_by(:name => 'technical_contact'))
     end
     it 'add privileges' do
-      test_service.set_roles_and_privileges test_user, test_manager, nil, [Role.find_by(:name => 'admin').id.to_s]
+      test_service.set_roles_and_privileges test_user, test_admin, nil, [Role.find_by(:name => 'admin').id.to_s]
       test_user.reload
 
       expect(test_user.roles).to include(Role.find_by(:name => 'admin'))
@@ -31,7 +54,7 @@ RSpec.describe UserRoleService, :type => :service do
 
       expect(test_user.roles).to include(Role.find_by(:name => 'technical_contact'))
 
-      test_service.set_roles_and_privileges test_user, test_manager, Role.find_by(:name => 'admin').id.to_s, []
+      test_service.set_roles_and_privileges test_user, test_admin, Role.find_by(:name => 'admin').id.to_s, []
       test_user.reload
 
       expect(test_user.roles).not_to include(Role.find_by(:name => 'technical_contact'))
@@ -41,13 +64,13 @@ RSpec.describe UserRoleService, :type => :service do
   it '.assign_role' do
     expect(test_user.roles).not_to include(Role.find_by(:name => 'technical_contact'))
 
-    test_service.send(:assign_role, test_user, Role.find_by(:name => 'technical_contact'), test_manager)
+    test_service.send(:assign_role, test_user, Role.find_by(:name => 'technical_contact'), test_admin)
     test_user.reload
 
     expect(test_user.roles).to include(Role.find_by(:name => 'technical_contact'))
 
     test_role = UsersRole.find_by(:user => test_user, :role => Role.find_by(:name => 'technical_contact'))
-    expect(test_role.granted_by_user).to eq(test_manager)
+    expect(test_role.granted_by_user).to eq(test_admin)
     expect(test_role.granted_on_date).to eq(Date.today)
     expect(test_role.active).to be true
   end
@@ -57,7 +80,7 @@ RSpec.describe UserRoleService, :type => :service do
 
       expect(test_user.roles).not_to include(Role.find_by(:name => 'admin'))
 
-      test_service.send(:revoke_role, test_user, Role.find_by(:name => 'admin'), test_manager)
+      test_service.send(:revoke_role, test_user, Role.find_by(:name => 'admin'), test_admin)
       test_user.reload
 
       expect(test_user.roles).not_to include(Role.find_by(:name => 'admin'))
@@ -68,7 +91,7 @@ RSpec.describe UserRoleService, :type => :service do
 
       expect(test_user.roles).to include(Role.find_by(:name => 'admin'))
 
-      test_service.send(:revoke_role, test_user, Role.find_by(:name => 'admin'), test_manager)
+      test_service.send(:revoke_role, test_user, Role.find_by(:name => 'admin'), test_admin)
       test_user.reload
 
       expect(test_user.roles).not_to include(Role.find_by(:name => 'admin'))
