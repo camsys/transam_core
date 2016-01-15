561798f0160818fd8f22a54cdff8e658623d5c89
Lydia Chang <mathmerized@gmail.com>
2016-01-15 12:51:20 -0500

    experiment with code climate

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'spec_helper'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
