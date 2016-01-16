e5e2ad8a56674caad205b2d85f3eef82f6f3abd1
Lydia Chang <mathmerized@gmail.com>
2016-01-16 12:24:01 -0500

    fix rspec maintenance update view

--- spec/views/asset_events/_maintenance_update_event_view.html.haml_spec.rb
+++ spec/views/asset_events/_maintenance_update_event_view.html.haml_spec.rb
@@ -9,13 +9,13 @@ describe "asset_events/_maintenance_update_event_view.html.haml", :type => :view
   it 'info' do
     test_asset = create(:buslike_asset)
     test_type = create(:maintenance_type)
-    test_asset.asset_events.create!(attributes_for(:maintenance_update_event, :maintenance_type_id => 1, :current_mileage => 44444, :comments => 'test comment 123'))
+    test_asset.asset_events.create!(attributes_for(:maintenance_update_event, :maintenance_type_id => test_type.id, :current_mileage => 44444, :comments => 'test comment 123'))
     test_event = AssetEvent.as_typed_event(test_asset.asset_events.last)
     assign(:asset, Asset.get_typed_asset(test_asset))
     assign(:asset_event, test_event)
     render
 
-    expect(rendered).to have_content(MaintenanceType.first.to_s)
+    expect(rendered).to have_content(test_type.to_s)
     expect(rendered).to have_content('44,444')
     expect(rendered).to have_content(Date.today.strftime('%m/%d/%Y'))
     expect(rendered).to have_content('test comment 123')
