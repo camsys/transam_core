5756bfc8777228e4c1037b0ed8a28a629953d0f1
Lydia Chang <mathmerized@gmail.com>
2016-01-16 12:25:10 -0500

    fix sql syntax error

--- app/models/maintenance_type.rb
+++ app/models/maintenance_type.rb
@@ -8,7 +8,7 @@ class MaintenanceType < ActiveRecord::Base
       x = where('name = ? OR description = ?', text, text).first
     else
       val = "%#{text}%"
-      x = where('name = LIKE ? OR description LIKE ?', val, val).first
+      x = where('name LIKE ? OR description LIKE ?', val, val).first
     end
     x
   end
