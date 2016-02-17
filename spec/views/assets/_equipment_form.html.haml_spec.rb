f693bec07f31de08f50a2740787c8fe28166221b
Lydia Chang <mathmerized@gmail.com>
2016-02-17 15:58:19 -0500

    update test for hiding eul in form

--- spec/views/assets/_equipment_form.html.haml_spec.rb
+++ spec/views/assets/_equipment_form.html.haml_spec.rb
@@ -23,6 +23,5 @@ describe "assets/_equipment_form.html.haml", :type => :view do
     expect(rendered).to have_field('asset_in_service_date')
     expect(rendered).to have_field('asset_purchased_new_true')
     expect(rendered).to have_field('vendor_name')
-    expect(rendered).to have_field('asset_expected_useful_life')
   end
 end
