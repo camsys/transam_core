cd9f52a11a856158236f588e7f81a018f71aa90a
Xudong Liu <xudong.camsys@gmail.com>
2016-03-18 18:20:10 -0400

    Only migrate when user_organization_filters table exists [#115770549]

--- db/migrate/20160318215151_add_sort_order_to_user_organization_filters.rb
+++ db/migrate/20160318215151_add_sort_order_to_user_organization_filters.rb
@@ -1,5 +1,7 @@
 class AddSortOrderToUserOrganizationFilters < ActiveRecord::Migration
   def change
-    add_column :user_organization_filters, :sort_order, :integer
+    if ActiveRecord::Base.connection.table_exists?(:user_organization_filters)
+      add_column :user_organization_filters, :sort_order, :integer 
+    end
   end
 end
