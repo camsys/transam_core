5b26ced510f07b75178845eb8b12dc30a574cdc9
Xudong Liu <xudong.camsys@gmail.com>
2016-03-31 15:44:36 -0400

    Not run early_dispositio data migration in test environment

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330130515) do
+ActiveRecord::Schema.define(version: 20160331192758) do
 
   create_table "activities", force: true do |t|
     t.string   "object_key",           limit: 12
