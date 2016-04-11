3e1450f10e46505226db2b3b378a71f7e465335e
Lydia Chang <mathmerized@gmail.com>
2016-04-11 13:37:44 -0400

    revert carrierwave dev config breaking download

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,27 +3,27 @@ CarrierWave.configure do |config|
   config.cache_dir = 'carrierwave' # ...two lines
 
   config.fog_credentials = {
-    :provider               => 'AWS',                       
+    :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
     :region                 => ENV['AWS_S3_REGION'],
-    :path_style            => true      
+    :path_style            => true
   } unless Rails.env.test? # no tests depend on this right now.  Just turn off fog
 
   # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.development? || Rails.env.cucumber?
+  if Rails.env.test? || Rails.env.cucumber?
     config.storage = :file
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp"
   else
     config.storage = :fog
-  end  
-  
+  end
+
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
- 
+
   config.fog_directory    = ENV['AWS_S3_BUCKET']
-  config.fog_use_ssl_for_aws = false  
+  config.fog_use_ssl_for_aws = false
   #config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-  #config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"  
-  
+  #config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+
 end
