f833dfbec3a97da8c0b4b945875f7fcd3cdaf3b6
Lydia Chang <mathmerized@gmail.com>
2016-04-27 14:59:16 -0400

    [fixes#117660619] devise already validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,7 +77,7 @@ class User < ActiveRecord::Base
   validates :external_id,   :allow_nil => true, :length => { maximum: 32 }
   validates :title,         :allow_nil => true, :length => { maximum: 64 }
 
-  validates :email,         :presence => true,  :length => { maximum: 128 },  :uniqueness => true, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "email address is not valid" }
+  #validates :email,         :presence => true,  :length => { maximum: 128 },  :uniqueness => true, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "email address is not valid" }
   validates :phone,         :presence => true,  :length => { maximum: 12 }
   validates :phone_ext,     :allow_nil => true, :length => { maximum: 6 }
   validates :timezone,      :presence => true,  :length => { maximum: 32 }
