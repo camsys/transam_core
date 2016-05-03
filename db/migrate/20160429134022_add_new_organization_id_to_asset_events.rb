86ee2b1b5a7c38d495a6055c04eb8954a51f166d
mmaranda-cs <mmaranda@camsys.com>
2016-05-03 08:21:49 -0400

    [#116538279] Updating the rake script so that it adds a column name that worked better with Ruby convention.

--- db/migrate/20160429134022_add_new_organization_id_to_asset_events.rb
+++ db/migrate/20160429134022_add_new_organization_id_to_asset_events.rb
@@ -1,5 +1,5 @@
 class AddNewOrganizationIdToAssetEvents < ActiveRecord::Migration
   def change
-    add_column :asset_events, :new_organization_id, :int, after: :comments
+    add_column :asset_events, :organization_id, :int, after: :comments
   end
 end
