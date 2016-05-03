3a4f083911fcd1b181671564cd653b70568ec0b9
mmaranda-cs <mmaranda@camsys.com>
2016-05-03 13:06:00 -0400

    [#116538279] Adding the transferred asset option to link directly to the edit screen.

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -35,7 +35,6 @@ class Equipment < Asset
     transferred_asset = self.copy
     org = Organization.where(:id => new_organization_id).first
 
-    transferred_asset.asset_tag = nil
     transferred_asset.fta_funding_type = nil
     transferred_asset.in_service_date = nil
     transferred_asset.organization = org
@@ -43,6 +42,9 @@ class Equipment < Asset
     transferred_asset.purchase_date = nil
     transferred_asset.service_status_type = nil
     transferred_asset.title_owner_organization = nil
+
+    transferred_asset.generate_object_key(:object_key)
+    transferred_asset.asset_tag = transferred_asset.object_key
 
     transferred_asset.save(:validate => false)
   end
