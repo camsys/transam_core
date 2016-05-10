bb533c9bb321b825217a95f2e51f4a25589a99cd
Xudong Liu <xudong.camsys@gmail.com>
2016-05-10 13:51:49 -0400

    Upgrade bootstrap-table to v1.10.1, and override its default silentSort option value as false so to indicate user of data loading [fixes#101699636]

--- app/assets/javascripts/transam_core.js
+++ app/assets/javascripts/transam_core.js
@@ -1,2 +1,6 @@
 //= require_tree .
-
+if($.fn.bootstrapTable) {
+  // override bootstrap table default option, set silentSort as false 
+  // so whenever sort, blank the table and display the loading message
+  $.fn.bootstrapTable.defaults.silentSort = false;
+}
