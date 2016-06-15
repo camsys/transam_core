709036156c45dd07ef0943c3fea36a9d7ac1fa0b
mmaranda-cs <mmaranda@camsys.com>
2016-06-15 08:11:44 -0400

    [#120456471] Renamed the migration and updated it to include a default for existing issues.

---
+++ db/migrate/20160610141958_add_resolution_columns_to_issue.rb
@@ -0,0 +1,25 @@
+class AddResolutionColumnsToIssue < ActiveRecord::Migration
+  def change
+
+    unless ActiveRecord::Base.connection.table_exists? 'issue_status_types'
+      create_table :issue_status_types, :force => true do |t|
+        t.string  :name,        :limit => 32,   :null => false
+        t.string  :description, :limit => 254,  :null => false
+        t.boolean  "active"
+      end
+    end
+
+    unless column_exists? :issues, :issue_status
+      add_column    :issues, :issue_status, :string, :after => :comments, :default => 'Open'
+    end
+    unless column_exists? :issues, :resolution_comments
+      add_column    :issues, :resolution_comments, :text, :after => :issue_status
+    end
+
+    issue_report = Report.find_by(id: 2)
+    issue_report.view_name = 'issues_report_table'
+    issue_report.custom_sql = "SELECT d.short_name AS 'ORGANIZATION', b.name AS 'TYPE', a.created_at AS 'DATE/TIME', a.comments AS 'COMMENTS', e.name AS 'BROWSER TYPE', c.first_name AS 'FIRST NAME', c.last_name AS 'LAST NAME', c.phone AS 'PHONE', a.issue_status AS 'ISSUE_STATUS' , a.resolution_comments AS 'RESOLUTION_COMMENTS', a.object_key AS 'ISSUE ID' FROM issues a LEFT JOIN issue_types b ON a.issue_type_id=b.id LEFT JOIN users c ON a.created_by_id=c.id LEFT JOIN organizations d ON c.organization_id=d.id LEFT JOIN web_browser_types e ON a.web_browser_type_id=e.id ORDER BY a.created_at"
+    issue_report.save
+
+  end
+end
