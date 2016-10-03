ef1875625aa8fdda6c7c5409c64b71bdc9e4e66d
mmaranda-cs <mmaranda@camsys.com>
2016-10-03 09:54:27 -0400

    [119079973] Updates the transam_format_helper to check if that date year equals 1 then it should not be displayed. This was simplified from creating a brand new date when comparing dates before display.

--- app/helpers/transam_format_helper.rb
+++ app/helpers/transam_format_helper.rb
@@ -215,9 +215,9 @@ module TransamFormatHelper
 
   def format_as_date(date, compact=true)
     if compact
-      date.strftime("%m/%d/%Y") unless (date.nil? || date == Date.new(1))
+      date.strftime("%m/%d/%Y") unless (date.nil? || date.year == 1)
     else
-      date.strftime("%b %d %Y") unless (date.nil? || date == Date.new(1))
+      date.strftime("%b %d %Y") unless (date.nil? || date.year == 1)
     end
   end
 
