49a977108413e71a8ecf398eba1e85f6eccec34d
Lydia Chang <mathmerized@gmail.com>
2016-11-01 15:17:58 -0400

    for migrations to filters check if filters table exists

--- db/migrate/20161005155224_add_role_to_user_organization_filters.rb
+++ db/migrate/20161005155224_add_role_to_user_organization_filters.rb
@@ -1,15 +1,17 @@
 class AddRoleToUserOrganizationFilters < ActiveRecord::Migration
   def change
-    add_reference :user_organization_filters, :created_by_user, index: true
-    add_column    :user_organization_filters, :query_string, :text
-    remove_column :user_organization_filters, :user_id
+    if ActiveRecord::Base.connection.table_exists?(:user_organization_filters)
+      add_reference :user_organization_filters, :created_by_user, index: true
+      add_column    :user_organization_filters, :query_string, :text
+      remove_column :user_organization_filters, :user_id
 
-    create_table :users_user_organization_filters do |t|
-      t.references :user,           null: false
-      t.references :user_organization_filter,   null: false
+      create_table :users_user_organization_filters do |t|
+        t.references :user,           null: false
+        t.references :user_organization_filter,   null: false
+      end
+
+      add_index :users_user_organization_filters, :user_id, name: 'users_user_organization_filters_idx1'
+      add_index :users_user_organization_filters, :user_organization_filter_id, name: 'users_user_organization_filters_idx2'
     end
-
-    add_index :users_user_organization_filters, :user_id, name: 'users_user_organization_filters_idx1'
-    add_index :users_user_organization_filters, :user_organization_filter_id, name: 'users_user_organization_filters_idx2'
   end
 end
--- db/migrate/20161011165614_add_resource_to_user_organization_filter.rb
+++ db/migrate/20161011165614_add_resource_to_user_organization_filter.rb
@@ -1,13 +1,17 @@
 class AddResourceToUserOrganizationFilter < ActiveRecord::Migration
   def up
-    change_table :user_organization_filters do |t|
-      t.references :resource, :polymorphic => true
+    if ActiveRecord::Base.connection.table_exists?(:user_organization_filters)
+      change_table :user_organization_filters do |t|
+        t.references :resource, :polymorphic => true
+      end
     end
   end
 
   def down
-    change_table :user_organization_filters do |t|
-      t.remove_references :resource, :polymorphic => true
+    if ActiveRecord::Base.connection.table_exists?(:user_organization_filters)
+      change_table :user_organization_filters do |t|
+        t.remove_references :resource, :polymorphic => true
+      end
     end
   end
 end
