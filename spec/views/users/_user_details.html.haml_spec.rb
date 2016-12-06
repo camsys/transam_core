af470112a03df89816f7a9a872403d46f76e80d2
Lydia Chang <mathmerized@gmail.com>
2016-12-06 13:38:32 -0500

    update test user data for spec for partial

--- spec/views/users/_user_details.html.haml_spec.rb
+++ spec/views/users/_user_details.html.haml_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe "users/_user_details.html.haml", :type => :view do
   it 'info' do
     test_user = create(:admin, :phone => 1234567890, :phone_ext => 111, :external_id => 'test_id', :address1 => '123 Main St', :city => 'Boston', :state => 'MA', :zip => '02140')
+    test_user.add_role :user
     render 'users/user_details', :user => test_user
 
     expect(rendered).to have_content(test_user.email)
