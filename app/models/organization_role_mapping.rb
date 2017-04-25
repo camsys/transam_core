25f62ee6cea9359db40e9481ddbc4de40eb31114
Lydia Chang <mathmerized@gmail.com>
2017-04-25 14:14:29 -0400

    [fixes#144214649] JS bugs on user form and dont cache org role mappings to pick up new orgs/mappings

--- app/models/organization_role_mapping.rb
+++ app/models/organization_role_mapping.rb
@@ -13,15 +13,11 @@ class OrganizationRoleMapping < ActiveRecord::Base
   
   # { org_id => role_id_array}, e.g. {1 => [1, 8, 9]}
   def self.role_ids_by_org
-    @org_role_ids ||= OrganizationRoleMapping.pluck(:organization_id, :role_id).each_with_object({}) { |(f,l),h|  h.update(f=>[l]) {|_,ov,nv| ov+nv }}
-    
-    @org_role_ids
+    OrganizationRoleMapping.pluck(:organization_id, :role_id).each_with_object({}) { |(f,l),h|  h.update(f=>[l]) {|_,ov,nv| ov+nv }}
   end
 
   # { role_id => org_id_array}, e.g. {1 => [1, 8, 9]}
   def self.org_ids_by_role
-    @role_org_ids ||= OrganizationRoleMapping.pluck(:role_id, :organization_id).each_with_object({}) { |(f,l),h|  h.update(f=>[l]) {|_,ov,nv| ov+nv }}
-    
-    @role_org_ids
+    OrganizationRoleMapping.pluck(:role_id, :organization_id).each_with_object({}) { |(f,l),h|  h.update(f=>[l]) {|_,ov,nv| ov+nv }}
   end
 end
