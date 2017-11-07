2b6deb86157137fa306a6ab66b596d79df8223b5
mathmerized <mathmerized@gmail.com>
2017-11-07 15:11:23 -0500

    dont call reporting engine columns

--- db/data_migrations/20171107175043_add_reporting_columns_core_reports.rb
+++
@@ -1,6 +0,0 @@
-class AddReportingColumnsCoreReports < ActiveRecord::DataMigration
-  def up
-    Report.find_by(name: 'User Login Report').update!(printable: true, exportable: true) if Report.find_by(name: 'User Login Report')
-    Report.find_by(name: 'Issues Report').update!(printable: true, exportable: true) if Report.find_by(name: 'Issues Report')
-  end
-end
