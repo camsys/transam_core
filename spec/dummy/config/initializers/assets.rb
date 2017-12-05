84d266ef6ec39beb7b65b6599dd49ab6167a9bdd
mathmerized <mathmerized@gmail.com>
2017-12-05 12:44:19 -0500

    add assets for cucumber tests

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+#temp add for cucumber tests
+Rails.application.config.assets.precompile += %w( logo.png )
