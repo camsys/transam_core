4367a2dc0f26a2bcc525599560865416d335bd12
mathmerized <mathmerized@gmail.com>
2018-02-28 14:57:21 -0500

    manufacturer seed cleanup

---
+++ db/data_migrations/20180228180011_update_equipment_manufacturers.rb
@@ -0,0 +1,18 @@
+class UpdateEquipmentManufacturers < ActiveRecord::DataMigration
+  def up
+
+    other_manufacturer = Manufacturer.find_or_create_by(filter: 'Equipment', name: 'Other (Describe)')
+
+    Equipment.where('manufacturer_id IS NOT NULL').each do |asset|
+      unless asset.manufacturer.name.include? 'Other'
+        asset.other_manufacturer = asset.manufacturer.name
+      end
+      asset.manufacturer = other_manufacturer
+
+      asset.save!
+    end
+
+    Manufacturer.where(filter: 'Equipment').where.not(id: other_manufacturer.id).delete_all
+
+  end
+end
