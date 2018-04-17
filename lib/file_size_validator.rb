a8dc830e798efd46207936856139775db956dc50
mathmerized <mathmerized@gmail.com>
2018-04-17 08:36:53 -0400

    fix setting in conditional as purposeful

--- lib/file_size_validator.rb
+++ lib/file_size_validator.rb
@@ -9,7 +9,7 @@ class FileSizeValidator < ActiveModel::EachValidator
   RESERVED_OPTIONS  = [:minimum, :maximum, :within, :is, :tokenizer, :too_short, :too_long]
  
   def initialize(options)
-    if range == (options.delete(:in) || options.delete(:within))
+    if range = (options.delete(:in) || options.delete(:within))
       raise ArgumentError, ":in and :within must be a Range" unless range.is_a?(Range)
       options[:minimum], options[:maximum] = range.begin, range.end
       options[:maximum] -= 1 if range.exclude_end?
