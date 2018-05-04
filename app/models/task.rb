e0bf35fefa936623cf7aeb28505426e83c55af77
mathmerized <mathmerized@gmail.com>
2018-05-04 14:52:39 -0400

    fix routes on polymorphic associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -173,7 +173,7 @@ class Task < ActiveRecord::Base
   def taskable_path
     return false if self.taskable.nil?
 
-    path = eval("Rails.application.routes.url_helpers.#{self.taskable.class.name.underscore}_path(id: '#{self.taskable.object_key}')")
+    path = eval("Rails.application.routes.url_helpers.#{self.taskable.class.base_class.name.underscore}_path(id: '#{self.taskable.object_key}')")
     begin
       Rails.application.routes.recognize_path(path)
     rescue
