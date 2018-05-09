23d85aa21384ed36ec10bc19001e65c250ae98d8
mathmerized <mathmerized@gmail.com>
2018-05-09 08:24:19 -0400

    move JS used everywhere to core JS asset file TTPLAT-98

--- app/assets/javascripts/transam_core.js
+++ app/assets/javascripts/transam_core.js
@@ -21,4 +21,35 @@ $(document).ready(function() {
         event.preventDefault();
         $(this).parent().find('input').datepicker('show');
     });
+
+
+    // install click handlers for navigable objects
+    transam.install_quick_link_handlers();
+
+    // Render any popovers and tooltips
+    transam.enable_info_popups('.info-icon');
+    transam.enable_info_popups('.transam-popover');
+    $('.transam-tooltip').tooltip();
+    $('table').on('post-body.bs.table', function () {
+        transam.enable_info_popups('.transam-popover');
+
+        $('.transam-tooltip').tooltip();
+    });
+
+
+    // Enable any date pickers etc
+    transam.enable_date_pickers();
+
+    // Force uppercase text for OFT inputs
+    $('[data-convert="uppercase"]').keyup(function(evt) {
+        this.value = this.value.toUpperCase();
+    });
+
+    // fiscal year fields
+    transam.make_fiscal_year('input.fiscal_year');
+
+    // Make the tabs responsive if the viewport gets smaller than the displayed tabs
+    $('.responsive-tabs').tabdrop({
+        text: '<i class="fa fa-align-justify"></i>'
+    });
 });
