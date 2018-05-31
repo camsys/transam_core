4caab542aac8985abe0d8c057b409386cbd87e5a
mathmerized <mathmerized@gmail.com>
2018-05-31 11:56:13 -0400

    remove disposition type default on asset event as unnecessary and new asset doesnt save disposition type

--- app/models/disposition_update_event.rb
+++ app/models/disposition_update_event.rb
@@ -59,7 +59,6 @@ class DispositionUpdateEvent < AssetEvent
   # Set resonable defaults for a new condition update event
   def set_defaults
     super
-    self.disposition_type ||= asset.disposition_type
     self.asset_event_type ||= AssetEventType.find_by_class_name(self.name)
   end
 
