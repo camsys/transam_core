744f1081c93ba571826c0cad4afb63d4964c4dda
mmaranda-cs <mmaranda@camsys.com>
2018-06-18 17:28:38 -0400

    TTPLAT-232 Ons econd thought maybe we don't want user_notifications scoped.

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -1,7 +1,7 @@
 class UserNotification < ActiveRecord::Base
 
   # Associations
-  belongs_to :user, -> { unscope(where: :active) }
+  belongs_to :user
   belongs_to :notification
 
   scope :unopened, -> { joins('INNER JOIN notifications ON notification_id = notifications.id').where('opened_at IS NULL AND notifications.active = true') }
