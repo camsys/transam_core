f00fc5a2c9428c18a06aea640618f0a6d9ad5296
W. Scott Meeks <smeeks@camsys.com>
2018-06-19 15:42:38 -0400

    Replace FactoryGirl for cucumber. [TTPLAT-152]

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,7 +10,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec/dummy/config/envi
 
 require 'cucumber/rails'
 
-require 'factory_girl_rails'
+require 'factory_bot_rails'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
