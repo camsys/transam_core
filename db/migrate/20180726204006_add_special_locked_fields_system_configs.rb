367c6321b6527d78f20e724be6295bb22a061fa4
mathmerized <mathmerized@gmail.com>
2018-07-27 10:37:32 -0400

    fix syntax error

--- db/migrate/20180726204006_add_special_locked_fields_system_configs.rb
+++ db/migrate/20180726204006_add_special_locked_fields_system_configs.rb
@@ -1,5 +1,5 @@
 class AddSpecialLockedFieldsSystemConfigs < ActiveRecord::Migration[5.2]
   def change
-    add_column :system_configs, :special_locked_fields, after: :data_file_path
+    add_column :system_configs, :special_locked_fields, :string, after: :data_file_path
   end
 end
