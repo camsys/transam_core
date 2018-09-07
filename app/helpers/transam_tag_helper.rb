0801ebced921f28292d1480f6794eacfc29d3aeb
W. Scott Meeks <smeeks@camsys.com>
2018-09-07 16:25:50 -0400

    Further refinements to association tag [TTPLAT-10]

--- app/helpers/transam_tag_helper.rb
+++ app/helpers/transam_tag_helper.rb
@@ -112,7 +112,8 @@ module TransamTagHelper
     
   def editable_asset_association_tag(asset, field, label=nil, collection=nil, current_method: nil, include_blank: false, current_value: nil, type: 'select', url: nil, suffix: '_id')
     # value = current_value || (collection || url ? asset.send(current_method || field).to_s : asset.send(current_method || "#{field.to_s}_id").to_s)
-    value = current_value || asset.send(current_method || "#{field.to_s}#{suffix}").to_s
+    field_name = current_method || "#{field.to_s.singularize}#{suffix}"
+    value = current_value || asset.send(current_method || field_name).to_s
     unless collection || url
       klass = asset.association(field).reflection.class_name.constantize
       collection = klass.column_names.include?('name') ?
@@ -131,7 +132,7 @@ module TransamTagHelper
   %label.control-label
     #{label || field.to_s.titleize}
   .display-value
-    %a.editable-field{href:'#', id: '#{field}', data: {name: 'asset[#{field}#{suffix}]', value: '#{value}', type: '#{type}', url: '#{asset_path(asset)}', source: \"#{url || "[#{source}]"}\", sourceCache: '#{url.nil?}'}}
+    %a.editable-field{href:'#', id: '#{field}', data: {name: 'asset[#{field_name}]', value: '#{value}', type: '#{type}', url: '#{asset_path(asset)}', source: \"#{url || "[#{source}]"}\", sourceCache: '#{url.nil?}'}}
 ")
     return engine.render.html_safe
   end
