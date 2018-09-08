f4b484a4bc9c39ac1ca3b3f42427cf52be96f764
mathmerized <mathmerized@gmail.com>
2018-09-08 18:45:04 -0400

    fix setting and getting asset events TTPLAT-8, add TransamAsset searchable fields

--- app/controllers/asset_events_controller.rb
+++ app/controllers/asset_events_controller.rb
@@ -174,8 +174,11 @@ class AssetEventsController < AssetAwareController
     # we need to know what the event type was for this event
     asset_event_type = AssetEventType.find(params[:event_type])
     unless asset_event_type.blank?
+      assoc_name = asset_event_type.class_name.gsub('Event', '').underscore.pluralize
+      assoc_name = 'early_disposition_requests' if assoc_name == 'early_disposition_request_updates'
+      id = @asset.send(assoc_name).proxy_association.owner.id
       asset_params = Hash.new
-      asset_params[Rails.application.config.asset_base_class_name.underscore] = ((@asset.type_of? Rails.application.config.asset_base_class_name.constantize) ? @asset : @asset.send(Rails.application.config.asset_base_class_name.underscore))
+      asset_params[Rails.application.config.asset_base_class_name.foreign_key] = id
       @asset_event = asset_event_type.class_name.constantize.new(form_params.merge(asset_params))
       @asset_event.creator = current_user
     end
