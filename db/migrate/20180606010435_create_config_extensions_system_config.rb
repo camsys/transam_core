d5f1c466cb0fd3bfb4a85d6a72965860157edee7
mathmerized <mathmerized@gmail.com>
2018-09-27 14:04:01 -0400

    Bump version and rename data migrations so theyre not the same class names as schema migrations

--- db/migrate/20180606010435_create_config_extensions_system_config.rb
+++ db/migrate/20180606010435_create_config_extensions_system_config.rb
@@ -1,4 +1,4 @@
-class AddConfigExtensionsSystemConfig < ActiveRecord::Migration[5.2]
+class CreateConfigExtensionsSystemConfig < ActiveRecord::Migration[5.2]
   def change
     add_column :system_configs, :default_weather_code, :string, after: :default_fiscal_year_formatter
   end
--- lib/transam_core/version.rb
+++ lib/transam_core/version.rb
@@ -1,3 +1,3 @@
 module TransamCore
-  VERSION = "2.3.6"
+  VERSION = "2.3.7"
 end
