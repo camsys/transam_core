d1eb6396e82230f74b0851bae6574ac1bde241aa
mathmerized <mathmerized@gmail.com>
2018-12-12 11:10:13 -0500

    comments

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -1,1 +1,5 @@
+# transam has many belongs_to associations that are not required
+# this is only necessary if the application loads defaults
+# it also has to be set after load_defaults
+
 Rails.application.config.active_record.belongs_to_required_by_default = false
