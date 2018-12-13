eded3b8e014bedcab170a62ee3a5440d6a9f66b6
Xudong Liu <xudong.camsys@gmail.com>
2018-12-13 16:35:13 -0500

    should be factory_bot

--- lib/transam_core/engine.rb
+++ lib/transam_core/engine.rb
@@ -64,7 +64,7 @@ module TransamCore
 
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.fixture_replacement :factory_bot, :dir => 'spec/factories'
       g.assets false
       g.helper false
     end
