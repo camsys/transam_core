588b3e750cc30b3def8605a4f202f3e4e3e14021
mathmerized <mathmerized@gmail.com>
2019-01-08 09:05:31 -0500

    syntax error

--- db/migrate/20190104145033_add_fields_images.rb
+++ db/migrate/20190104145033_add_fields_images.rb
@@ -2,7 +2,7 @@ class AddFieldsImages < ActiveRecord::Migration[5.2]
   def change
     add_reference :images, :base_imagable, polymorphic: true, after: :object_key
     add_column :images, :name, :string, after: :image
-    add_column :classification, :string, after: :image
+    add_column :images, :classification, :string, after: :image
     add_column :images, :exportable, :boolean, after: :description
   end
 end
