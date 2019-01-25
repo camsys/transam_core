d63159e13a030b4746907c853a9576c5cc3f98da
Alex Andrade <aandrade@camsys.com>
2019-01-25 11:46:14 -0500

    Fix failing core test.

--- spec/factories/policies.rb
+++ spec/factories/policies.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
 
-  factory :policy do
-    association :organization, :factory => :organization
+  trait :basic_policy_attributes do
     interest_rate { "0.05" }
     service_life_calculation_type_id { 1 }
     cost_calculation_type_id { 1 }
@@ -13,4 +12,41 @@ FactoryBot.define do
     current { true }
     active { true }
   end
+
+  factory :policy do
+    basic_policy_attributes
+    association :organization, :factory => :organization
+  end
+
+  factory :transam_policy, :class => :policy do
+    basic_policy_attributes
+    association :organization, :factory => :organization_basic
+  end
+
+  factory :parent_policy, :class => :policy do
+    basic_policy_attributes
+    association :organization, :factory => :organization_basic
+
+    transient do
+      has_fuel_type false
+    end
+
+    transient do
+      type 0
+    end
+
+    transient do
+      subtype 0
+    end
+
+    trait :fuel_type do
+      has_fuel_type true
+    end
+
+    after(:create) do |policy, evaluator|
+      create(:policy_transam_asset_type_rule, policy: policy, asset_type_id: evaluator.type)
+      create(:policy_transam_asset_subtype_rule, (:fuel_type if evaluator.has_fuel_type), policy: policy, asset_subtype_id: evaluator.subtype)
+    end
+  end
+
 end
--- spec/factories/policy_asset_subtype_rules.rb
+++ spec/factories/policy_asset_subtype_rules.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
 
-  factory :policy_asset_subtype_rule do
-    association :asset_subtype
+  trait :policy_asset_subtype_rule_attributes do
     min_service_life_months { 144 }
     min_service_life_miles { 500000 }
     replacement_cost { 2000 }
@@ -10,5 +9,19 @@ FactoryBot.define do
     replace_with_leased { false }
     purchase_replacement_code { 'XXX' }
     rehabilitation_code { 'XXX' }
+  end
+
+  factory :policy_asset_subtype_rule do
+    policy_asset_subtype_rule_attributes
+    association :asset_subtype
+  end
+
+  factory :policy_transam_asset_subtype_rule, :class => :policy_asset_subtype_rule do
+    policy_asset_subtype_rule_attributes
+    default_rule { true }
+
+    trait :fuel_type do
+      fuel_type_id { 18 }
+    end
   end
 end
