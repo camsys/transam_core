660c2c5b4223a119bed10a7052aef98ab3311ca7
mathmerized <mathmerized@gmail.com>
2019-03-08 14:42:12 -0500

    TTPLAT-250 TTPLAT-226 additional highway model fields and GUID column

---
+++ app/models/concerns/transam_guid.rb
@@ -0,0 +1,33 @@
+#------------------------------------------------------------------------------
+#
+# TransamGuid
+#
+# Adds a unique GUID to a model
+#
+#------------------------------------------------------------------------------
+module TransamGuid
+  
+  extend ActiveSupport::Concern
+
+  included do
+    # Always generate a unique GUID before saving to the database
+    before_validation(:on => :create) do
+      generate_guid(:guid)
+    end
+
+    validates :guid, :presence => true, :uniqueness => true
+  end
+
+  # Only generate an GUID if one is not set. This allows objects to be
+  # replaced but maintain the same GUID
+  def generate_guid(column)
+    if self[column].blank?
+      begin
+        self[column] = SecureRandom.uuid
+      end
+    end
+  end
+
+  protected
+
+end
