2fd1a071b12a649b98e6ad804a36c901be1f567b
Alex Andrade <aandrade@camsys.com>
2019-03-26 10:55:33 -0400

    Intermediary commit to change branches locally.

--- db/data_migrations/20190322202843_update_condition_event_assessed_ratings_and_condition_percents.rb
+++ db/data_migrations/20190322202843_update_condition_event_assessed_ratings_and_condition_percents.rb
@@ -1,5 +1,13 @@
 class UpdateConditionEventAssessedRatingsAndConditionPercents < ActiveRecord::DataMigration
   def up
+    ConditionUpdateEvent.joins(:base_transam_asset).where("transam_assets.purchased_new = 1 AND asset_events.event_date < transam_assets.purchase_date")
+
+    ConditionUpdateEvent.where(transam_asset: purchased_new).each do |e|
+      if e.event_date < e.transam_asset.try(:purchase_date)
+        e.update_(event_date: e.transam_asset.purchase_date)
+      end
+    end
+
     ConditionUpdateEvent.all.each do |e|
       e.update!(condition_type: ConditionType.from_rating(e.assessed_rating))
       ConditionTypePercent.where(asset_event_id: e.id).each do |p|
