2a004555e95047a6711a0eb6ef10615c312e38dc
Alex Andrade <aandrade@camsys.com>
2019-03-27 12:55:13 -0400

    [TTPLAT-525] Make final changes to condition type migrations for asset_events.

--- app/models/condition_update_event.rb
+++ app/models/condition_update_event.rb
@@ -19,8 +19,8 @@ class ConditionUpdateEvent < AssetEvent
   validates :assessed_rating,
       :presence     => true,
       :numericality => {
-      :greater_than_or_equal_to => ConditionType.minimum(:rating_ceiling) || 0,
-      :less_than_or_equal_to    => ConditionType.maximum(:rating_ceiling) || 5
+      :greater_than_or_equal_to => ConditionType.min_rating || 0,
+      :less_than_or_equal_to    => ConditionType.max_rating || 5
       },
       :allow_nil    => true
   # validates :comments, :length => {:maximum => ???} # There is no limit in the Database/Schema
--- db/data_migrations/20190322202843_update_condition_event_assessed_ratings_and_condition_percents.rb
+++ db/data_migrations/20190322202843_update_condition_event_assessed_ratings_and_condition_percents.rb
@@ -1,22 +1,18 @@
 class UpdateConditionEventAssessedRatingsAndConditionPercents < ActiveRecord::DataMigration
   def up
     ConditionUpdateEvent.joins(:base_transam_asset).where("transam_assets.purchased_new = 1 AND asset_events.event_date < transam_assets.purchase_date")
-
-    ConditionUpdateEvent.where(transam_asset: purchased_new).each do |e|
-      if e.event_date < e.transam_asset.try(:purchase_date)
-        e.update_(event_date: e.transam_asset.purchase_date)
+      .each do |e|
+        e.update_columns(event_date: e.transam_asset.purchase_date)
       end
+
+
+    ConditionUpdateEvent.joins(:base_transam_asset).each do |e|
+      e.update_columns(condition_type_id: ConditionType.from_rating(e.assessed_rating).id)
     end
 
-    ConditionUpdateEvent.all.each do |e|
-      e.update!(condition_type: ConditionType.from_rating(e.assessed_rating))
-      ConditionTypePercent.where(asset_event_id: e.id).each do |p|
-        if p.condition_type_id == e.condition_type_id
-          p.update!(pcnt: 100)
-        else
-          p.update!(pcnt: 0)
-        end
-      end
-    end
+    ConditionTypePercent.joins(:condition_update_event).where("condition_type_percents.condition_type_id = asset_events.condition_type_id")
+      .update_all(pcnt: 100)
+    ConditionTypePercent.joins(:condition_update_event).where.not("condition_type_percents.condition_type_id = asset_events.condition_type_id")
+      .update_all(pcnt: 0)
   end
 end
