8b503f618fb47c347cfb345d4039c6bed9382681
mathmerized <mathmerized@gmail.com>
2019-04-22 09:13:07 -0400

    TTPLAT-896 fix query to get rules with AND service life calcluation types

--- app/jobs/asset_service_life_update_job.rb
+++ app/jobs/asset_service_life_update_job.rb
@@ -11,7 +11,7 @@ class AssetServiceLifeUpdateJob < ActivityJob
     # Service life calculators with 'AND mileage/condition' could possibly set policy replacement year = planning year + 1
     # as planning year changes at rollover a background job has to run to update these values
     # set this up as an activity to run once a month but activity can be changed to run as often as you want to see if the 'AND' condition has been met changing the policy replacement year
-    rules = PolicyAssetTypeRule.where('class_name LIKE ?', "%#{'And'}%").where.not(policy_id: Policy.find_by(parent_id: nil).id)
+    rules = PolicyAssetTypeRule.joins(:service_life_calculation_type).where('service_life_calculation_types.class_name LIKE ?', "%#{'And'}%").where.not(policy_id: Policy.find_by(parent_id: nil).id)
     rules.each do |rule|
       assets = Rails.application.config.asset_base_class_name.constantize.joins(asset_subtype: :asset_type).where(organization_id: rule.policy.organization_id, asset_types: {id: rule.asset_type_id})
       assets.each do |asset|
