8e4db7688bfe6028fddb1304ce402a3cf9f409aa
mathmerized <mathmerized@gmail.com>
2019-04-23 10:22:39 -0400

    TTPLAT-896 service life activities only applicable to operational assets

--- app/jobs/asset_service_life_update_job.rb
+++ app/jobs/asset_service_life_update_job.rb
@@ -13,7 +13,7 @@ class AssetServiceLifeUpdateJob < ActivityJob
     # set this up as an activity to run once a month but activity can be changed to run as often as you want to see if the 'AND' condition has been met changing the policy replacement year
     rules = PolicyAssetTypeRule.joins(:service_life_calculation_type).where('service_life_calculation_types.class_name LIKE ?', "%#{'And'}%").where.not(policy_id: Policy.find_by(parent_id: nil).id)
     rules.each do |rule|
-      assets = Rails.application.config.asset_base_class_name.constantize.joins(asset_subtype: :asset_type).where(organization_id: rule.policy.organization_id, asset_types: {id: rule.asset_type_id})
+      assets = Rails.application.config.asset_base_class_name.constantize.operational.joins(asset_subtype: :asset_type).where(organization_id: rule.policy.organization_id, asset_types: {id: rule.asset_type_id})
       assets.each do |asset|
         asset.save! # trigger callbacks
       end
