529204bb30e3f8b5d9e3f6a3a7a9f3ff2e75622d
mathmerized <mathmerized@gmail.com>
2019-05-17 08:00:33 -0400

    TTPLAT-964 original report is CustomSqlReport. New report has its own class but never finished

--- app/reports/user_login_report.rb
+++ app/reports/user_login_report.rb
@@ -6,7 +6,8 @@ class UserLoginReport < AbstractReport
 
   def get_data(organization_id_list, params)
 
-    User.where("organization_id IN (?) AND last_sign_in_at IS NOT NULL", organization_id_list).order(:organization_id, :first_name, :last_name)
+
+    {data: User.joins(:organization).where("organization_id IN (?) AND last_sign_in_at IS NOT NULL", organization_id_list).order(:organization_id, :first_name, :last_name).pluck('organizations.short_name', :first_name, :last_name, :sign_in_count, :last_sign_in_at, :locked_at)}
 
   end
 
