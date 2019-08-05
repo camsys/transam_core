b05a2d6f7e3b37de9de0dd2eaeb017744649dfdd
mathmerized <mathmerized@gmail.com>
2019-08-05 10:11:11 -0400

    TTPLAT-777 fix defensive code

--- app/jobs/issues_report_job.rb
+++ app/jobs/issues_report_job.rb
@@ -10,33 +10,35 @@ class IssuesReportJob < ActivityJob
 
   def run
 
-    system_user = User.where(first_name: 'system', last_name: 'user').first
-
-    event_url = Rails.application.routes.url_helpers.report_url(Report.find_by(name: 'Issues Report'))
-
-    # send message to all admin about new issues created this week
-    activity = Activity.find_by(job_name: self.class.name)
-    frequency = activity.frequency_type.name
-    new_issues = Issue.where(issue_status_type: IssueStatusType.find_by(name: 'Open')).where('created_at >= ?', Date.today - (activity.frequency_quantity).send(frequency))
-
     message_template = MessageTemplate.find_by(name: 'Support1', active: true)
-    message_body = MessageTemplateMessageGenerator.new.generate(message_template, [new_issues.count, frequency, "<a href='#{event_url}'>here</a>"])
+    if message_template.present?
+      system_user = User.where(first_name: 'system', last_name: 'user').first
 
-    if new_issues.count > 0 && message_template.present?
-      User.with_role(:admin).each do |user|
-        msg               = Message.new
-        msg.user          = system_user
-        msg.organization  = system_user.organization
-        msg.to_user       = user
-        msg.subject       = message_template.subject
-        msg.body          = message_body
-        msg.priority_type = message_template.priority_type
-        msg.message_template = message_template
-        msg.save
+      event_url = Rails.application.routes.url_helpers.report_url(Report.find_by(name: 'Issues Report'))
+
+      # send message to all admin about new issues created this week
+      activity = Activity.find_by(job_name: self.class.name)
+      frequency = activity.frequency_type.name
+      new_issues = Issue.where(issue_status_type: IssueStatusType.find_by(name: 'Open')).where('created_at >= ?', Date.today - (activity.frequency_quantity).send(frequency))
+
+      message_body = MessageTemplateMessageGenerator.new.generate(message_template, [new_issues.count, frequency, "<a href='#{event_url}'>here</a>"])
+
+      if new_issues.count > 0
+        User.with_role(:admin).each do |user|
+          msg               = Message.new
+          msg.user          = system_user
+          msg.organization  = system_user.organization
+          msg.to_user       = user
+          msg.subject       = message_template.subject
+          msg.body          = message_body
+          msg.priority_type = message_template.priority_type
+          msg.message_template = message_template
+          msg.save
+        end
       end
-    end
 
-    write_to_activity_log system_user.organization, "Issues Report sent to admin users"
+      write_to_activity_log system_user.organization, "Issues Report sent to admin users"
+    end
 
   end
 
