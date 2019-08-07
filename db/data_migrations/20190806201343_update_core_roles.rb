e641e8ce3ac492fe81d8ebaa5f4279e5db2a6756
Alex Andrade <aandrade@camsys.com>
2019-08-07 16:46:42 -0400

    [TTPLAT-1044] Remove parent assignment from super manager role.

--- db/data_migrations/20190806201343_update_core_roles.rb
+++ db/data_migrations/20190806201343_update_core_roles.rb
@@ -1,7 +1,7 @@
 class UpdateCoreRoles < ActiveRecord::DataMigration
   def up
     Role.where(name: [:user, :manager]).update_all(label: 'Staff')
-    Role.find_by(name: :super_manager).update(label: 'Grantor Manager', privilege: false)
+    Role.find_by(name: :super_manager).update(label: 'Grantor Manager', privilege: false, role_parent_id: nil)
   end
 
   def down
