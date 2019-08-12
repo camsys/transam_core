0edc8f908a4763dae01817cd1bae4e3fc2b12537
mathmerized <mathmerized@gmail.com>
2019-08-12 19:15:52 -0400

    spelling error

--- db/data_migrations/20190812195314_force_run_service_life_update_job.rb
+++ db/data_migrations/20190812195314_force_run_service_life_update_job.rb
@@ -1,6 +1,6 @@
 class ForceRunServiceLifeUpdateJob < ActiveRecord::DataMigration
   def up
-    if SystemCongfigExtension.find_by(extension_name: 'PolicyAware', active: true)
+    if SystemConfigExtension.find_by(extension_name: 'PolicyAware', active: true)
       rules = PolicyAssetTypeRule.joins(:service_life_calculation_type).where('service_life_calculation_types.class_name LIKE ?', "%#{'And'}%").where.not(policy_id: Policy.find_by(parent_id: nil).id)
       rules.each do |rule|
         assets = Rails.application.config.asset_base_class_name.constantize.operational.joins(asset_subtype: :asset_type).where(organization_id: rule.policy.organization_id, asset_types: {id: rule.asset_type_id})
