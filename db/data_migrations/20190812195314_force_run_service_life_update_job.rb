0c4d839e1029fc9e29d7cc25de22db09a9f7032d
mathmerized <mathmerized@gmail.com>
2019-08-12 18:21:03 -0400

    dont run service life updates unless policy aware extension is on

--- db/data_migrations/20190812195314_force_run_service_life_update_job.rb
+++ db/data_migrations/20190812195314_force_run_service_life_update_job.rb
@@ -1,10 +1,12 @@
 class ForceRunServiceLifeUpdateJob < ActiveRecord::DataMigration
   def up
-    rules = PolicyAssetTypeRule.joins(:service_life_calculation_type).where('service_life_calculation_types.class_name LIKE ?', "%#{'And'}%").where.not(policy_id: Policy.find_by(parent_id: nil).id)
-    rules.each do |rule|
-      assets = Rails.application.config.asset_base_class_name.constantize.operational.joins(asset_subtype: :asset_type).where(organization_id: rule.policy.organization_id, asset_types: {id: rule.asset_type_id})
-      assets.each do |asset|
-        asset.save! # trigger callbacks
+    if SystemCongfigExtension.find_by(extension_name: 'PolicyAware', active: true)
+      rules = PolicyAssetTypeRule.joins(:service_life_calculation_type).where('service_life_calculation_types.class_name LIKE ?', "%#{'And'}%").where.not(policy_id: Policy.find_by(parent_id: nil).id)
+      rules.each do |rule|
+        assets = Rails.application.config.asset_base_class_name.constantize.operational.joins(asset_subtype: :asset_type).where(organization_id: rule.policy.organization_id, asset_types: {id: rule.asset_type_id})
+        assets.each do |asset|
+          asset.save! # trigger callbacks
+        end
       end
     end
   end
