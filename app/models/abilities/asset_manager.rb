3cfd549550e21d3729541967045b7250a105e6de
Alex Andrade <aandrade@camsys.com>
2019-08-14 10:24:03 -0400

    [TTPLAT-1050] Modify asset manager privilege.

--- app/models/abilities/asset_manager.rb
+++ app/models/abilities/asset_manager.rb
@@ -6,17 +6,8 @@ module Abilities
 
       # Only allow users to dispose of assets that are disposable and that they
       # own
-      can :dispose, Asset do |a|
-        (DispositionUpdateEvent.asset_event_type.try(:active) && a.disposable?(true) && user.organization_ids.include?(a.organization_id))
-      end
-
       can :dispose, TransamAssetRecord do |a|
         (DispositionUpdateEvent.asset_event_type.try(:active) && a.disposable?(true) && user.organization_ids.include?(a.organization_id))
-      end
-
-      # can manage asset events if the asset's organization is in their list
-      can :manage, AssetEvent do |ae|
-        ae.asset_event_type.try(:active) && user.organization_ids.include?(ae.send(Rails.application.config.asset_base_class_name.underscore).try(:organization_id))
       end
 
       can :manage, EarlyDispositionRequestUpdateEvent do |ae|
