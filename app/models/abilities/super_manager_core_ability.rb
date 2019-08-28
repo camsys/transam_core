6ae9f057395466c9cd4a70baae489d57c9bc787d
mathmerized <mathmerized@gmail.com>
2019-08-28 17:32:18 -0400

    TTPLAT-1050 fix ordering and copying of asset manager abilities

--- app/models/abilities/super_manager_core_ability.rb
+++ app/models/abilities/super_manager_core_ability.rb
@@ -2,13 +2,7 @@ module Abilities
   class SuperManagerCoreAbility
     include CanCan::Ability
 
-    def initialize(user)
-
-      ['Asset', 'AssetEvent'].each do |c|
-        ability = "Abilities::SuperManager#{c}Ability".constantize.new(user)
-
-        self.merge ability if ability.present?
-      end
+    def initialize(user, organization_ids=[])
 
       can :assign, Role do |r|
         r.name != "admin"
