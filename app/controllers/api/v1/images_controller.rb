b0d82d98e0ed9cc1e0116e22a42b8dd7c400b77a
W. Scott Meeks <smeeks@camsys.com>
2019-08-30 13:42:08 -0400

    BMCDOT-423 allow form_params to be overridden

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -50,6 +50,13 @@ class Api::V1::ImagesController < Api::V1::NestedResourceController
     end
   end
 
+  protected
+  
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def form_params
+    params.permit(Image.allowable_params)
+  end
+
   private
 
   def set_imagable
@@ -70,11 +77,6 @@ class Api::V1::ImagesController < Api::V1::NestedResourceController
       @data = {id: "Image #{params[:id]} not found."}
       render status: :not_found, json: json_response(:fail, data: @data)
     end
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def form_params
-    params.permit(Image.allowable_params)
   end
 
 end
