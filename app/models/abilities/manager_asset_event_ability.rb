d9bc672f659d1d1447fce93ee73c867ca0b64756
mathmerized <mathmerized@gmail.com>
2019-11-18 14:56:06 -0500

    TTPLAT-1221 granter staff can't manage early dispositions

--- app/models/abilities/manager_asset_event_ability.rb
+++ app/models/abilities/manager_asset_event_ability.rb
@@ -12,10 +12,6 @@ module Abilities
         ae.asset_event_type.try(:active) && user.organization_ids.include?(ae.send(Rails.application.config.asset_base_class_name.underscore).try(:organization_id))
       end
 
-      can :manage, EarlyDispositionRequestUpdateEvent do |ae|
-        ae.asset_event_type.try(:active)
-      end
-
       cannot :create, DispositionUpdateEvent do |ae|
         !ae.send(Rails.application.config.asset_base_class_name.underscore).try(:disposable?,true)
       end
