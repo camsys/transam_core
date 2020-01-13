3653809fac675998d683cbc4edf30b1db2f8b918
mathmerized <mathmerized@gmail.com>
2020-01-13 13:58:20 -0500

    add postgres syntax to sql file just in case

--- db/core_asset_query_views.sql
+++ db/core_asset_query_views.sql
@@ -7,3 +7,10 @@ SELECT transam_assets.id AS parent_id, transam_assets.asset_tag, transam_assets.
 CONCAT(asset_tag, IF(description IS NOT NULL, ' : ', ''), IFNULL(description,'')) AS parent_name
 FROM `transam_assets`
 WHERE transam_assets.id IN (SELECT DISTINCT parent_id FROM transam_assets WHERE parent_id IS NOT NULL)
+
+-- postgis
+-- CREATE OR REPLACE VIEW parent_transam_assets_view AS
+--   SELECT transam_assets.id AS parent_id, transam_assets.asset_tag, transam_assets.description,
+--   CONCAT(asset_tag, CASE WHEN description IS NOT NULL THEN ' : ' ELSE '' END, description) AS parent_name
+--   FROM transam_assets
+--   WHERE transam_assets.id IN (SELECT DISTINCT parent_id FROM transam_assets WHERE parent_id IS NOT NULL)
