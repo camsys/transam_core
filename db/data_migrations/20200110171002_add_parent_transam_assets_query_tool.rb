3fb3e1235473946b7965e02007237c52224585c4
mathmerized <mathmerized@gmail.com>
2020-01-13 13:54:22 -0500

    add syntax for postgres view

--- db/data_migrations/20200110171002_add_parent_transam_assets_query_tool.rb
+++ db/data_migrations/20200110171002_add_parent_transam_assets_query_tool.rb
@@ -1,13 +1,23 @@
 class AddParentTransamAssetsQueryTool < ActiveRecord::DataMigration
   def up
 
-    parent_transam_assets_view_sql = <<-SQL
-       CREATE OR REPLACE VIEW parent_transam_assets_view AS
-SELECT transam_assets.id AS parent_id, transam_assets.asset_tag, transam_assets.description,
-CONCAT(asset_tag, IF(description IS NOT NULL, ' : ', ''), IFNULL(description,'')) AS parent_name
-FROM transam_assets
-WHERE transam_assets.id IN (SELECT DISTINCT parent_id FROM transam_assets WHERE parent_id IS NOT NULL)
-    SQL
+    if ActiveRecord::Base.configurations[Rails.env]['adapter'].include? 'mysql2'
+      parent_transam_assets_view_sql = <<-SQL
+         CREATE OR REPLACE VIEW parent_transam_assets_view AS
+  SELECT transam_assets.id AS parent_id, transam_assets.asset_tag, transam_assets.description,
+  CONCAT(asset_tag, IF(description IS NOT NULL, ' : ', ''), IFNULL(description,'')) AS parent_name
+  FROM transam_assets
+  WHERE transam_assets.id IN (SELECT DISTINCT parent_id FROM transam_assets WHERE parent_id IS NOT NULL)
+      SQL
+    elsif ActiveRecord::Base.configurations[Rails.env]['adapter'].include? 'post'
+      parent_transam_assets_view_sql = <<-SQL
+         CREATE OR REPLACE VIEW parent_transam_assets_view AS
+  SELECT transam_assets.id AS parent_id, transam_assets.asset_tag, transam_assets.description,
+  CONCAT(asset_tag, CASE WHEN description IS NOT NULL THEN ' : ' ELSE '' END, description) AS parent_name
+  FROM transam_assets
+  WHERE transam_assets.id IN (SELECT DISTINCT parent_id FROM transam_assets WHERE parent_id IS NOT NULL)
+      SQL
+    end
     ActiveRecord::Base.connection.execute parent_transam_assets_view_sql
 
 
