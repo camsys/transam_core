7065197764bd080c3be8111322598d284e8d9ee7
Alex Andrade <aandrade@camsys.com>
2020-02-04 15:53:34 -0500

    Only update updated_by_id of asset events whose created_by_id are not null.

--- db/data_migrations/20200203210751_populate_updated_by_id_for_asset_events.rb
+++ db/data_migrations/20200203210751_populate_updated_by_id_for_asset_events.rb
@@ -1,6 +1,6 @@
 class PopulateUpdatedByIdForAssetEvents < ActiveRecord::DataMigration
   def up
-    AssetEvent.where(updated_by_id: nil).each do |e|
+    AssetEvent.where.not(created_by_id: nil).where(updated_by_id: nil).each do |e|
       e.update_column(:updated_by_id, e.created_by_id)
     end
   end
