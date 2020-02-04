23828aecaf75312543052b25c54827f56da1f1a2
Alex Andrade <aandrade@camsys.com>
2020-02-04 13:22:09 -0500

    Update core and transit query view sql scripts.

--- db/core_asset_query_views.sql
+++ db/core_asset_query_views.sql
@@ -6,17 +6,24 @@ CREATE OR REPLACE VIEW parent_transam_assets_view AS
 SELECT transam_assets.organization_id, transam_assets.id AS parent_id, transam_assets.asset_tag, transam_assets.description,
 CONCAT(asset_tag, IF(description IS NOT NULL, ' : ', ''), IFNULL(description,'')) AS parent_name
 FROM `transam_assets`
-WHERE transam_assets.id IN (SELECT DISTINCT parent_id FROM transam_assets WHERE parent_id IS NOT NULL)
+WHERE transam_assets.id IN (SELECT DISTINCT parent_id FROM transam_assets WHERE parent_id IS NOT NULL);
 
 DROP VIEW if exists query_tool_most_recent_asset_events_for_type_view;
 CREATE OR REPLACE VIEW query_tool_most_recent_asset_events_for_type_view AS
-SELECT
-    aet.id AS asset_event_type_id, aet.name AS asset_event_name, Max(ae.created_at) AS asset_event_created_time,
-    ae.base_transam_asset_id, Max(ae.id) AS asset_event_id
+SELECT aet.id AS asset_event_type_id, aet.name AS asset_event_name, Max(ae.created_at) AS asset_event_created_time,
+       ae.base_transam_asset_id, Max(ae.id) AS asset_event_id
 FROM asset_events AS ae
 LEFT JOIN asset_event_types AS aet ON aet.id = ae.asset_event_type_id
 LEFT JOIN transam_assets AS ta  ON ta.id = ae.base_transam_asset_id
 GROUP BY aet.id, ae.base_transam_asset_id;
+
+DROP VIEW if exists most_recent_asset_events_updated_by_user_view;
+CREATE OR REPLACE VIEW most_recent_asset_events_updated_by_user_view AS
+SELECT mrae.asset_event_id, mrae.base_transam_asset_id, CONCAT(u.first_name, " ", u.last_name) AS event_by
+FROM query_tool_most_recent_asset_events_for_type_view AS mrae
+LEFT JOIN asset_events AS ae ON ae.id = mrae.asset_event_id
+LEFT JOIN transam_assets AS ta  ON ta.id = mrae.base_transam_asset_id
+LEFT JOIN users AS u ON u.id = ae.updated_by_id;
 
 -- postgis
 -- CREATE OR REPLACE VIEW parent_transam_assets_view AS
