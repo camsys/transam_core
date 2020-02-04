89cbdbd8c02488f35961b08c72f7a06ea22c72a1
Alex Andrade <aandrade@camsys.com>
2020-02-04 13:02:56 -0500

    Move query tool most recent asset events view and associated fields to core.

--- db/core_asset_query_views.sql
+++ db/core_asset_query_views.sql
@@ -8,6 +8,16 @@ CONCAT(asset_tag, IF(description IS NOT NULL, ' : ', ''), IFNULL(description,'')
 FROM `transam_assets`
 WHERE transam_assets.id IN (SELECT DISTINCT parent_id FROM transam_assets WHERE parent_id IS NOT NULL)
 
+DROP VIEW if exists query_tool_most_recent_asset_events_for_type_view;
+CREATE OR REPLACE VIEW query_tool_most_recent_asset_events_for_type_view AS
+SELECT
+    aet.id AS asset_event_type_id, aet.name AS asset_event_name, Max(ae.created_at) AS asset_event_created_time,
+    ae.base_transam_asset_id, Max(ae.id) AS asset_event_id
+FROM asset_events AS ae
+LEFT JOIN asset_event_types AS aet ON aet.id = ae.asset_event_type_id
+LEFT JOIN transam_assets AS ta  ON ta.id = ae.base_transam_asset_id
+GROUP BY aet.id, ae.base_transam_asset_id;
+
 -- postgis
 -- CREATE OR REPLACE VIEW parent_transam_assets_view AS
 --   SELECT transam_assets.organization_id, transam_assets.id AS parent_id, transam_assets.asset_tag, transam_assets.description,
--- db/seeds/asset_query_seeds.rb
+++ db/seeds/asset_query_seeds.rb
@@ -4,6 +4,23 @@ puts "======= Loading core asset query configurations ======="
 # transam_assets table
 QueryAssetClass.find_or_create_by(table_name: 'transam_assets')
 
+most_recent_view_sql = <<-SQL
+      CREATE OR REPLACE VIEW query_tool_most_recent_asset_events_for_type_view AS
+        SELECT
+          aet.id AS asset_event_type_id, aet.name AS asset_event_name, Max(ae.created_at) AS asset_event_created_time,
+          ae.base_transam_asset_id, Max(ae.id) AS asset_event_id
+        FROM asset_events AS ae
+        LEFT JOIN asset_event_types AS aet ON aet.id = ae.asset_event_type_id
+        LEFT JOIN transam_assets AS ta  ON ta.id = ae.base_transam_asset_id
+        GROUP BY aet.id, ae.base_transam_asset_id;
+SQL
+ActiveRecord::Base.connection.execute most_recent_view_sql
+
+most_recent_asset_events_table = QueryAssetClass.find_or_create_by(
+    table_name: 'most_recent_asset_events',
+    transam_assets_join: "left join query_tool_most_recent_asset_events_for_type_view mraev on mraev.base_transam_asset_id = transam_assets.id left join asset_events as most_recent_asset_events on most_recent_asset_events.id = mraev.asset_event_id left join asset_event_types as mrae_types on most_recent_asset_events.asset_event_type_id = mrae_types.id"
+)
+
 # Query Category and fields
 transam_assets_category_fields = {
   'Identification & Classification': [
@@ -174,9 +191,29 @@ transam_assets_category_fields = {
   ]
 }
 
+most_recent_asset_events_fields = {
+  "Life Cycle (History Log)": [
+    {
+      name: 'event_date',
+      label: 'Event Date',
+      filter_type: 'date'
+    },
+    {
+      name: 'comments',
+      label: 'Comments',
+      filter_type: 'text'
+    },
+    {
+      name: 'updated_at',
+      label: 'Entry Date & Time',
+      filter_type: 'date'
+    }
+  ]
+}
 # seeding
 fields_data = {
-  'transam_assets': transam_assets_category_fields
+  'transam_assets': transam_assets_category_fields,
+  'most_recent_asset_events': most_recent_asset_events_fields
 }
 
 fields_data.each do |table_name, category_fields|
