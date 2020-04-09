ee85c07e936b11547ea239954c10fe19149ec794
mathmerized <mathmerized@gmail.com>
2020-04-09 14:10:31 -0400

    TTPLAT-1475 only call active asset event types in API

--- app/controllers/api/v1/asset_event_types_controller.rb
+++ app/controllers/api/v1/asset_event_types_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AssetEventTypesController < Api::ApiController
   def index
     event_types = []
-    AssetEventType.all.each do |event_type|
+    AssetEventType.active.each do |event_type|
       event_types << {name: event_type.name, id: event_type.id, allowable_params: event_type.class_name.constantize.allowable_params} 
     end
     render json: json_response(:success, data: event_types)
