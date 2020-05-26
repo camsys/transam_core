640ca586e481ef48ee274168883f77a916c78046
NJBL <beriolebeau.n@husky.neu.edu>
2020-05-26 13:41:09 -0400

    explicit pixels on checkbox width

--- app/assets/javascripts/transam_core/table-library/main.js
+++ app/assets/javascripts/transam_core/table-library/main.js
@@ -80,7 +80,7 @@ function updateHeader(id, selected, cols, col_ts){
     let header = $('<tr>').addClass("header");
     let colgroup = $('<colgroup>');
     header.append($('<th>').addClass("header-item header-checkbox").append($('<label>').append($('<input>').attr('type', "checkbox").addClass("header-checkbox")).append($('<span>').addClass('fa-stack').append($('<i class="fad fa-square fa-stack-1x" aria-hidden="true"></i>')).append($('<i class="fas fa-check-square fa-stack-1x" aria-hidden="true"></i>')))));
-    colgroup.append($('<col>').addClass('col-item').attr('style', 'width: 2.5em'));
+    colgroup.append($('<col>').addClass('col-item').attr('style', 'width: 24px'));
     for (let i=0;i<selected.length;i++) {
         let colIndex = cols.indexOf(selected[i].toString().trim());
         try {
