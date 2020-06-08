ce92bc2f28cfd38e5425c8e298230c6556a0f362
NJBL <beriolebeau.n@husky.neu.edu>
2020-06-08 17:31:21 -0400

    update comparator to handle both numbers and strings

--- app/assets/javascripts/transam_core/table-library/sorting.js
+++ app/assets/javascripts/transam_core/table-library/sorting.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(document).on("click",".header-item",(function(){
         var table = $(this).parents('.elbat').eq(0);
+        $(this).siblings().attr("order", "");
         var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index(), $(this).attr("order")));
         $(this).attr("order", $(this).attr("order") == "ascending" ? "descending" : "ascending");
         //todo: update the sort_params object
@@ -8,18 +9,12 @@ $(document).ready(function(){
     })
 )});
 
-function comparer(index, order) {
-    return function(a, b) {
-        var valA = getCellValue(a, index), valB = getCellValue(b, index);
-        if (order == "ascending") {
-            return valA - valB;
-        } else {
-            return valB - valA;
-        }
-        
-    }
-}
+const comparer = (idx, order) => (a, b) => ((v1, v2) => (order == "ascending") ?
+                      v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2) :
+                      v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v2 - v1 : v2.toString().localeCompare(v1))
+                    (getCellValue(a, idx), getCellValue(b, idx));
+
+
 function getCellValue(row, index){
-    // todo build cases for each data type
     return $(row).children('td').eq(index).text();
 }
