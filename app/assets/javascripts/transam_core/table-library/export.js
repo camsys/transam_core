cd14e5a6c9436121e1391cb38cd9eb0ef08f9bb8
NJBL <beriolebeau.n@husky.neu.edu>
2020-07-14 19:27:04 -0400

    full server side export and improved support for exporting multiple times

--- app/assets/javascripts/transam_core/table-library/export.js
+++ app/assets/javascripts/transam_core/table-library/export.js
@@ -1,14 +1,27 @@
-$(document).on('click', ".export_option", function(event){
+$(document).on('click', ".export_option", async function(event){
   event.stopPropagation();
+  $(this).addClass('loading');
   const id = $(this).closest(".library-table").find("table").eq(0).attr('id');
+  const href = await export_table(id, $(this).text().trim());
   if($(this).text().trim() === "csv") {
-    $(this).attr({ 'download': "output.csv", 'href': export_table(id, $(this).text().trim()), 'target': '_blank' });
+    $(this).append($('<a class="hidden-link">').attr({ 'download': "output.csv", 'href': href, 'target': '_blank' }));
   } else if($(this).text().trim() === "txt") {
-    $(this).attr({ 'download': "output.txt", 'href': export_table(id, $(this).text().trim()), 'target': '_blank' });
+    $(this).append($('<a class="hidden-link">').attr({ 'download': "output.txt", 'href': href, 'target': '_blank' }));
   } else if($(this).text().trim() === "excel") {
-    $(this).attr({ 'download': "output.xls", 'href': export_table(id, $(this).text().trim()), 'target': '_blank' });
+    $(this).append($('<a class="hidden-link">').attr({ 'download': "output.xls", 'href': href, 'target': '_blank' }));
   }
+  let hidden = $(this).find(".hidden-link")[0];
+  hidden.click()
+  hidden.remove();
+
+
   $(this).closest('.export').eq(0).removeClass("open");
+  $(this).removeClass('loading');
+
+});
+
+$(document).on('click', ".hidden-link", (e) => {
+  e.stopPropagation();
 });
 
 
@@ -18,7 +31,14 @@ $(document).on('click', ".export_option", function(event){
 async function export_table(id, type) {
   const table = $('#'+id);
   const side = $(table).data('side');
+  const url = $(table).data('url');
+  const table_code = $(table).data('tableCode');
   const header = table.find("tr:has(th)").eq(0).find(".header-item:not(.header-checkbox)");
+  let csv = "";
+  for(let cell of header){
+    csv = csv + $(cell).find(".header-text").text().replace(/,/g, '') + ",";
+  }
+  csv = csv + '\n';
   let rows = [];
   if(side === "client") {
     if($('#' + id + '_export_checkbox').is(':checked')) {
@@ -26,11 +46,7 @@ async function export_table(id, type) {
     } else {
       rows = table.find("tr:has(td)");
     }
-    let csv = "";
-    for(let cell of header){
-      csv = csv + $(cell).find(".header-text").text().replace(/,/g, '') + ",";
-    }
-    csv = csv + '\n';
+    
     for(let row of rows){
       for(let c of $(row).find(".row-item")) {
         if($(c).hasClass("action-column")) {
@@ -44,9 +60,10 @@ async function export_table(id, type) {
       csv = csv + '\n';
     }    
   } else {
-    let csv = "";
-    let data = {'page': 0, 'page_size': 22222, 'search': $('#'+id).siblings(".function_bar").find(".searchbar").val()}; // 22222 page size is meant to include all of the rows, ugly hack
-    sorted_column = $(header).children(".sorted");
+    let data = {"table_code":table_code, 'page': 0, 'page_size': 22222, 'search': $('#'+id).siblings(".function_bar").find(".searchbar").val()}; // 22222 page size is meant to include all of the rows, ugly hack
+    const params = $(table).data('params');
+    for(let x in params){ data[x] = params[x]; }
+    sorted_column = $(table).find(".header-item.sorted").eq(0);
     data['sort_column'] = $(sorted_column).attr("code");
     data['sort_order'] = ($(sorted_column).hasClass("sorted-desc")) ? "descending": "ascending";
     await $.ajax({
@@ -59,30 +76,34 @@ async function export_table(id, type) {
         response = d;
       },
       complete: (jqXHR, status) => {  
-        let rows = [];
         if(status == 'success') {
           r = response;
-          r_columns = Object.keys(r['rows'][0]); 
+          try {
+            r_columns = Object.keys(r['rows'][0]); 
+          } catch(e) {
+            console.error("empty table?", e);
+            return;
+          }
+          
           for(let [index,obj] of r['rows'].entries()) {
-            let row = {};
             let columns = Object.keys(obj);
             for(let col of columns) {
-              row[col] = obj[col]["data"];
+              csv = csv + (""+obj[col]["data"]).toString().replace(/,/g, '') + ",";
             }
-            rows.append(row);
+            csv = csv + '\n';
           }
         }
-        console.log(rows);
       },
       error: function (e){
           console.log(e);
           return -1;
       }
     });
+
   }
 
 
-
+  let csvData = "";
   if(type === "csv") {
     csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
     return csvData;
@@ -121,7 +142,7 @@ function init_export(id, types) {
 
 
 // close on click away
-$(document).on('click', function(e){
+$(document).on('click', 'body > *:not(.selected_rows_checkbox_label)', function(e){
   if($(e.target).closest('.open').length === 0) {
     $('.open').removeClass('open');
   }
