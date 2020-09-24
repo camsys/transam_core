f7c4799c2ea1c21c4986c7043576b825e92f0753
Alex Andrade <aandrade@camsys.com>
2020-09-24 14:54:52 -0400

    [BMCDOT-819] Correct migration to remove document_tag_id from documents table

--- db/migrate/20200924183148_remove_document_tag_from_documents.rb
+++ db/migrate/20200924183148_remove_document_tag_from_documents.rb
@@ -1,5 +1,5 @@
 class RemoveDocumentTagFromDocuments < ActiveRecord::Migration[5.2]
   def change
-    remove_column :documents, :document_tag
+    remove_column :documents, :document_tag_id
   end
 end
