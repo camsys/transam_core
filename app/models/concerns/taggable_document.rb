3f4f1c863efb9943bbb1cbcb4e04aff1fb5dd8ad
Alex Andrade <aandrade@camsys.com>
2020-10-13 16:59:57 -0400

    [BMCDOT-825] Add table for structure documents sorted by folder. Current tab functionality is not working, but the table works correctly if you force it to become visible (by adding the "active in" classes to the tab-pane).

--- app/models/concerns/taggable_document.rb
+++ app/models/concerns/taggable_document.rb
@@ -3,11 +3,20 @@ module TaggableDocument
 
   included do
     belongs_to :document_tag
-
-    # returns boolean indicating whether or not update was successful
-    def update_from_filename filename
-      self.original_filename = filename
-      self.valid?
-    end
+    validates :document_tag_id, :presence => true
   end
+
+  attr_accessor :attributes_from_file_name
+  attr_accessor :folder
+
+  def self.form_params
+    FORM_PARAMS + :document_tag_id
+  end
+
+  # returns boolean indicating whether or not update was successful
+  def update_from_filename filename
+    self.original_filename = filename
+    self.valid?
+  end
+
 end
