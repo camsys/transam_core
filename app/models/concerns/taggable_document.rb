99088a30815f130076f945f187cafe47818fd29d
W. Scott Meeks <smeeks@camsys.com>
2020-10-20 12:10:25 -0400

    BMCDOT-819 Correct implementation of allowable_params override

--- app/models/concerns/taggable_document.rb
+++ app/models/concerns/taggable_document.rb
@@ -3,15 +3,11 @@ module TaggableDocument
 
   included do
     belongs_to :document_tag
-    validates :document_tag_id, :presence => true
+    singleton_class.prepend ClassOverrides
   end
 
   attr_accessor :attributes_from_file_name
   attr_accessor :folder
-
-  def self.form_params
-    FORM_PARAMS + :document_tag_id
-  end
 
   # returns boolean indicating whether or not update was successful
   def update_from_filename filename
@@ -19,4 +15,10 @@ module TaggableDocument
     self.valid?
   end
 
+  module ClassOverrides
+    def allowable_params
+      Document::FORM_PARAMS << :document_tag_id
+    end
+  end
+    
 end
