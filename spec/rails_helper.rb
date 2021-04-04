46194c33b718f0f5b7f0170956684ba23e52f987
W. Scott Meeks <smeeks@camsys.com>
2021-04-04 09:10:40 -0400

    Add AWS_REGION for transam_controller
    For some reason I didn't need this locally, but I think this should fix Travis.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+ENV['AWS_REGION'] ||= 'us-east-1' # For aws-sdk-s3
 
 require 'codacy-coverage'
 require 'simplecov'
