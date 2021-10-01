3abbec60d9953cc8874a730da59637afbd436a21
Alex Andrade <aandrade@camsys.com>
2021-10-01 17:55:51 -0400

    [Bulk Updates] Support Asset Lifecycle Events - Mileage, Condition, Service Status

--- app/models/service_status_type.rb
+++ app/models/service_status_type.rb
@@ -20,4 +20,13 @@ class ServiceStatusType < ActiveRecord::Base
     as_json(options)
   end
 
+  # for bulk updates
+  def self.schema_structure
+    {
+        "enum": ServiceStatusType.all.pluck(:name),
+        "tuple": ServiceStatusType.all.map{ |x| {"id": x.id, "val": x.name} },
+        "type": "string"
+    }
+  end
+
 end
